0924cf8e302e364a830f2693efc00846e6585c26
Jesse Shapiro <jesse@opencraft.com>
2017-03-27 10:00:08 -0400

    Add segment.io tracking to data sharing consent

---
+++ spec/vendor/analytics-mock.js
@@ -0,0 +1,15 @@
+var lastTrackedEvent = "";
+
+var lastTrackedForm = {}
+
+var analytics = {
+    track: function (eventName) {
+        lastTrackedEvent = eventName
+    },
+    trackForm: function (form, callback) {
+        lastTrackedForm = {
+            form: form,
+            callback: callback
+        }
+    },
+};
