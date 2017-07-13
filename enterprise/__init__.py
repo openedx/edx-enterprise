ef053d29d057934104357ecc4ea159fddc7b19bf
Jesse Shapiro <jesse@opencraft.com>
2017-07-13 09:19:52 -0400

    Revert versions of packages too new for edx-platform

--- enterprise/__init__.py
+++ enterprise/__init__.py
@@ -4,6 +4,6 @@ Your project description goes here.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "0.38.0"
+__version__ = "0.38.1"
 
 default_app_config = "enterprise.apps.EnterpriseConfig"  # pylint: disable=invalid-name
---
+++ integrated_channels/sap_success_factors/migrations/0007_remove_historicalsapsuccessfactorsenterprisecustomerconfiguration_history_change_reason.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sap_success_factors', '0006_sapsuccessfactors_use_enterprise_enrollment_page_waffle_flag'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='historicalsapsuccessfactorsenterprisecustomerconfiguration',
+            name='history_change_reason',
+        ),
+    ]
