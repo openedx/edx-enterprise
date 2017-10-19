b2bd795ea03e0f8364abfeb405faa3a11d0a8959
Uman Shahzad <uman@opencraft.com>
2017-10-19 18:34:29 +0500

    Remove EnterpriseIntegratedChannels model.
    It was unused and does not seem to be useful.

--- integrated_channels/integrated_channel/models.py
+++ integrated_channels/integrated_channel/models.py
@@ -7,50 +7,12 @@ from __future__ import absolute_import, unicode_literals
 import logging
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
 
 from model_utils.models import TimeStampedModel
 
 from enterprise.models import EnterpriseCustomer
 
 LOGGER = logging.getLogger(__name__)
-
-
-@python_2_unicode_compatible
-class EnterpriseIntegratedChannel(TimeStampedModel):
-    """
-    Store information related to integrating with external enterprise systems.
-
-    EnterpriseIntegratedChannel is an external system that the Enterprise Customer uses to manage enterprise
-    related data. Each of these requires an edx plugin in order to send data to these systems, this model stores
-    information related to what and where to send data from the enterprise app.
-    """
-
-    name = models.CharField(max_length=255, blank=False, null=False, help_text=_("Third Party name."))
-    data_type = models.CharField(max_length=100, blank=False, null=False, help_text=_("Data Type"))
-
-    class Meta:
-        app_label = 'integrated_channel'
-        verbose_name = _("Enterprise Integrated Channel")
-        verbose_name_plural = _("Enterprise Integrated Channels")
-        unique_together = (("name", "data_type"),)
-
-    def __str__(self):
-        """
-        Return human-readable string representation.
-        """
-        return "<EnterpriseIntegratedChannel {name} for {data_type} data with id {id}>".format(
-            name=self.name,
-            data_type=self.data_type,
-            id=self.id
-        )
-
-    def __repr__(self):
-        """
-        Return uniquely identifying string representation.
-        """
-        return self.__str__()
 
 
 class EnterpriseCustomerPluginConfiguration(TimeStampedModel):
