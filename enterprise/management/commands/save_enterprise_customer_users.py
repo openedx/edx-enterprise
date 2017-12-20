81f0996c77d11cff5561529f78beabf249254ea6
Douglas Hall <douglashall@dhcp-18-189-69-227.dyn.mit.edu>
2017-12-20 10:46:16 -0500

    ENT-799 Add save_enterprise_customer_users command.
    We need to be able to back-populate enterprise data on
    SailThru users. The easiest way to do this is to run
    through all EnterpriseCustomerUsers and save them which
    will cause a signal receiver in LMS to fire off tasks
    for updating the SailThru user via the SailThru API.

---
+++ enterprise/management/commands/save_enterprise_customer_users.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Django management command for saving EnterpriseCustomerUser models.
+"""
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+from django.core.management import BaseCommand
+
+from enterprise.models import EnterpriseCustomerUser
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Calls save() on EnterpriseCustomerUser models.
+
+    This is useful for triggering save-related signals causing the
+    associated signal receiver functions to fire.
+    """
+    help = 'Save existing EnterpriseCustomerUser models.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-e',
+            '--enterprise_customer_uuid',
+            action='store',
+            dest='enterprise_customer_uuid',
+            default=None,
+            help='Run this command for only the given EnterpriseCustomer UUID.'
+        )
+
+    def handle(self, *args, **options):
+        enterprise_customer_filter = {}
+        enterprise_customer_uuid = options.get('enterprise_customer_uuid')
+        if enterprise_customer_uuid:
+            enterprise_customer_filter['enterprise_customer'] = enterprise_customer_uuid
+
+        count = 0
+        for enterprise_customer_user in EnterpriseCustomerUser.objects.filter(**enterprise_customer_filter):
+            enterprise_customer_user.save()
+            count += 1
+
+        LOGGER.info('%s EnterpriseCustomerUser models saved.', count)
