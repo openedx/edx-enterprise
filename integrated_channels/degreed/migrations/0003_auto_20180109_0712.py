668bfae93d1c0ebf8b3b082656dd65ebbaaf6c37
Saleem Latif <saleem_ee@hotmail.com>
2018-01-09 18:15:48 +0500

    Add missing migrations for degreed and sap_success_factors

---
+++ integrated_channels/degreed/migrations/0003_auto_20180109_0712.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('degreed', '0002_auto_20180104_0103'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='degreedenterprisecustomerconfiguration',
+            name='active',
+            field=models.BooleanField(help_text='Is this configuration active?'),
+        ),
+        migrations.AlterField(
+            model_name='historicaldegreedenterprisecustomerconfiguration',
+            name='active',
+            field=models.BooleanField(help_text='Is this configuration active?'),
+        ),
+    ]
