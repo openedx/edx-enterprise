05f6af8957344cecdb1362cc7089a1ba93831070
Uman Shahzad <uman@opencraft.com>
2018-05-01 12:53:24 +0500

    Add new fields to make data reporting more generic.

---
+++ enterprise/migrations/0043_auto_20180501_0008.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('enterprise', '0042_replace_sensitive_sso_username'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='enterprisecustomerreportingconfiguration',
+            name='data_type',
+            field=models.CharField(default='progress', help_text='The type of data this report should contain.', max_length=20, verbose_name='Data Type', choices=[('progress', 'progress'), ('catalog', 'catalog')]),
+        ),
+        migrations.AddField(
+            model_name='enterprisecustomerreportingconfiguration',
+            name='report_type',
+            field=models.CharField(default='csv', help_text='The type this report should be sent as, e.g. CSV.', max_length=20, verbose_name='Report Type', choices=[('csv', 'csv')]),
+        ),
+    ]
