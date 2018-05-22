0406474a2e2147d996f010f37563efe5780f0482
asadiqbal <aiqbal@edx.org>
2018-05-22 13:25:17 +0500

    Order by enterprise customer name

--- enterprise/__init__.py
+++ enterprise/__init__.py
@@ -4,6 +4,6 @@ Your project description goes here.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "0.68.6"
+__version__ = "0.68.7"
 
 default_app_config = "enterprise.apps.EnterpriseConfig"  # pylint: disable=invalid-name
