94ee98a1a1735627e7de9293e542d9c3f9e2fe27
muhammad-ammar <mammar@gmail.com>
2018-05-28 12:18:48 +0500

    update EnterpriseCustomerCatalog model and admin
    ENT-897

--- enterprise/models.py
+++ enterprise/models.py
@@ -1029,7 +1029,7 @@ class EnterpriseCustomerCatalog(TimeStampedModel):
     )
     title = models.CharField(
         default='All Content',
-        max_length=20,
+        max_length=255,
         blank=False,
         null=False
     )
