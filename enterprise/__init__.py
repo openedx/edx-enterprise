dc45da10321568a9e60931f66cf328fa5c6aa2dc
Saleem Latif <saleem_ee@hotmail.com>
2018-07-09 16:26:09 +0500

    Updated tincan API package to support python 3

--- enterprise/__init__.py
+++ enterprise/__init__.py
@@ -4,6 +4,6 @@ Your project description goes here.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "0.70.8"
+__version__ = "0.71.0"
 
 default_app_config = "enterprise.apps.EnterpriseConfig"  # pylint: disable=invalid-name
