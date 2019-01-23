876b3db184bdea1f741d5d1f4d75e4b92ec18e85
Troy Sankey <tsankey@edx.org>
2019-01-23 15:27:51 -0500

    bump version to 1.2.9
    PLAT-2387

--- enterprise/__init__.py
+++ enterprise/__init__.py
@@ -4,6 +4,6 @@ Your project description goes here.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "1.2.8"
+__version__ = "1.2.9"
 
 default_app_config = "enterprise.apps.EnterpriseConfig"  # pylint: disable=invalid-name
