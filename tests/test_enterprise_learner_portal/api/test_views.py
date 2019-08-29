76830bfc2c51e15ec47f010cb54c49adc10c2ec5
Brittney Exline <bexline@edx.org>
2019-08-29 13:11:41 -0600

    ENT-2203 Update learner portal enterprise enrollments endpoint to filter on enterprise_id

--- tests/test_enterprise_learner_portal/api/test_views.py
+++ tests/test_enterprise_learner_portal/api/test_views.py
@@ -5,9 +5,12 @@ Tests for the EnterpriseCourseEnrollmentview of the enterprise_learner_portal ap
 
 from __future__ import absolute_import, unicode_literals
 
+import uuid
+
 import mock
 from pytest import mark
 
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import Client, TestCase
 
@@ -22,10 +25,18 @@ class TestEnterpriseCourseEnrollmentView(TestCase):
     EnterpriseCourseEnrollmentView tests.
     """
 
+    class MockSerializer(object):
+        """ Returns obj with data property. """
+
+        @property
+        def data(self):
+            """ Return fake serialized data. """
+            return {'hooray': 'here is the data'}
+
     def setUp(self):
         super(TestEnterpriseCourseEnrollmentView, self).setUp()
 
-        enterprise_customer = factories.EnterpriseCustomerFactory()
+        self.enterprise_customer = factories.EnterpriseCustomerFactory()
 
         # Create our user we will enroll in a course
         self.user = factories.UserFactory.create(is_staff=True, is_active=True)
@@ -33,7 +44,7 @@ class TestEnterpriseCourseEnrollmentView(TestCase):
         self.user.save()
         enrolled_ent_customer_user = factories.EnterpriseCustomerUserFactory(
             user_id=self.user.id,
-            enterprise_customer=enterprise_customer,
+            enterprise_customer=self.enterprise_customer,
         )
         course_run_id = 'course-v1:edX+DemoX+Demo_Course'
         EnterpriseCourseEnrollment.objects.create(
@@ -47,7 +58,7 @@ class TestEnterpriseCourseEnrollmentView(TestCase):
         )
         factories.EnterpriseCustomerUserFactory(
             user_id=not_enrolled_user.id,
-            enterprise_customer=enterprise_customer,
+            enterprise_customer=self.enterprise_customer,
         )
 
         self.client = Client()
@@ -60,25 +71,66 @@ class TestEnterpriseCourseEnrollmentView(TestCase):
         View should return data created by EnterpriseCourseEnrollmentSerializer
         (which we mock in this case)
         """
-        class MockSerializer(object):
-            """ Returns obj with data property. """
-            @property
-            def data(self):
-                """ Return fake serialized data. """
-                return {'hooray': 'here is the data'}
 
         mock_get_overviews.return_value = {'overview_info': 'this would be a larger dict'}
-        mock_serializer.return_value = MockSerializer()
+        mock_serializer.return_value = self.MockSerializer()
 
-        url = reverse('enterprise-learner-portal-course-enrollment-list')
-        resp = self.client.get(url)
+        resp = self.client.get(
+            '{host}{path}?enterprise_id={enterprise_id}'.format(
+                host=settings.TEST_SERVER,
+                path=reverse('enterprise-learner-portal-course-enrollment-list'),
+                enterprise_id=str(self.enterprise_customer.uuid)
+            )
+        )
         assert resp.status_code == 200
         assert resp.json() == {'hooray': 'here is the data'}
+
+    @mock.patch('enterprise_learner_portal.api.v1.views.EnterpriseCourseEnrollmentSerializer')
+    @mock.patch('enterprise_learner_portal.api.v1.views.get_course_overviews')
+    def test_view_returns_bad_request_without_enterprise(self, mock_get_overviews, mock_serializer):
+        """
+        View should return a 400 because of the missing enterprise_id parameter.
+        """
+        mock_get_overviews.return_value = {'overview_info': 'this would be a larger dict'}
+        mock_serializer.return_value = self.MockSerializer()
+
+        resp = self.client.get(
+            '{host}{path}'.format(
+                host=settings.TEST_SERVER,
+                path=reverse('enterprise-learner-portal-course-enrollment-list')
+            )
+        )
+        assert resp.status_code == 400
+        assert resp.json() == {'error': 'enterprise_id must be provided as a query parameter'}
+
+    @mock.patch('enterprise_learner_portal.api.v1.views.EnterpriseCourseEnrollmentSerializer')
+    @mock.patch('enterprise_learner_portal.api.v1.views.get_course_overviews')
+    def test_view_returns_not_found_unlinked_enterprise(self, mock_get_overviews, mock_serializer):
+        """
+        View should return a 404 because the user is not linked to the enterprise.
+        """
+        mock_get_overviews.return_value = {'overview_info': 'this would be a larger dict'}
+        mock_serializer.return_value = self.MockSerializer()
+
+        resp = self.client.get(
+            '{host}{path}?enterprise_id={enterprise_id}'.format(
+                host=settings.TEST_SERVER,
+                path=reverse('enterprise-learner-portal-course-enrollment-list'),
+                enterprise_id=str(uuid.uuid4()),
+            )
+        )
+        assert resp.status_code == 404
+        assert resp.json() == {'detail': 'Not found.'}
 
     def test_view_requires_openedx_installation(self):
         """
         View should raise error if imports to helper methods fail.
         """
-        url = reverse('enterprise-learner-portal-course-enrollment-list')
         with self.assertRaises(NotConnectedToOpenEdX):
-            self.client.get(url)
+            self.client.get(
+                '{host}{path}?enterprise_id={enterprise_id}'.format(
+                    host=settings.TEST_SERVER,
+                    path=reverse('enterprise-learner-portal-course-enrollment-list'),
+                    enterprise_id=str(self.enterprise_customer.uuid)
+                )
+            )
