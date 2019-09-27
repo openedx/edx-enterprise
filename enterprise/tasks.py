570080f50530d22f59cce1c03bebc5173388ad5c
Christopher Pappas <cpappas@edx.org>
2019-09-27 18:08:18 -0400

    ENT-2310 | Adding a post_save receiver to listen to CourseEnrollments table
    Adding in a test file I forgot. Updating a few test doctrings
    
    Actually adding in the tasks file too
    
    Updating task to prevent duplicate recreation of EnterpriseCourseEnrollment
    
    Appeasing pylint
    
    bumping version

---
+++ enterprise/tasks.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Django tasks.
+"""
+from __future__ import absolute_import, unicode_literals
+
+from logging import getLogger
+
+from celery import shared_task
+
+from enterprise.models import EnterpriseCourseEnrollment
+
+LOGGER = getLogger(__name__)
+
+
+@shared_task
+def create_enterprise_enrollment(course_id, enterprise_customer_user):
+    """
+    Create enterprise enrollment for user if course_id part of catalog for the ENT customer.
+    """
+    # Prevent duplicate records from being created if possible
+    # before we need to make a call to discovery
+    if EnterpriseCourseEnrollment.objects.filter(
+            enterprise_customer_user=enterprise_customer_user,
+            course_id=course_id,
+    ).exists():
+        LOGGER.info((
+            "EnterpriseCourseEnrollment record exists for user %s "
+            "on course %s. Exiting task."
+        ), enterprise_customer_user.user_id, course_id)
+        return
+
+    enterprise_customer = enterprise_customer_user.enterprise_customer
+    if enterprise_customer.catalog_contains_course(course_id):
+        LOGGER.info((
+            "Creating EnterpriseCourseEnrollment for user %s "
+            "on course %s for enterprise_customer %s"
+        ), enterprise_customer_user.user_id, course_id, enterprise_customer)
+        EnterpriseCourseEnrollment.objects.create(
+            course_id=course_id,
+            enterprise_customer_user=enterprise_customer_user,
+        )
