c0b8423d47aa893a890caa88593e0fb1b929f09c
irfanuddinahmad <irfan.ahmad@arbisoft.com>
2020-03-05 17:53:25 +0500

    added new report data type

--- enterprise/models.py
+++ enterprise/models.py
@@ -1783,10 +1783,12 @@ class EnterpriseCustomerReportingConfiguration(TimeStampedModel):
     DATA_TYPE_PROGRESS = 'progress'  # Refers to gathering progress data from Vertica (to be deprecated)
     DATA_TYPE_PROGRESS_V2 = 'progress_v2'  # Refers to gathering progress data from the Analytics Data API
     DATA_TYPE_CATALOG = 'catalog'
+    DATA_TYPE_ENGAGEMENT = 'engagement'  # Refers to gathering engagement data from the Analytics Data API
     DATA_TYPE_CHOICES = (
         (DATA_TYPE_PROGRESS, DATA_TYPE_PROGRESS),
         (DATA_TYPE_PROGRESS_V2, DATA_TYPE_PROGRESS_V2),
         (DATA_TYPE_CATALOG, DATA_TYPE_CATALOG),
+        (DATA_TYPE_ENGAGEMENT, DATA_TYPE_ENGAGEMENT),
     )
 
     REPORT_TYPE_CSV = 'csv'
