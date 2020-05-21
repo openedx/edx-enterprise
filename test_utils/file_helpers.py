a623a735bd6628f4b3615d22e24cbe2a8e0dc931
Sankar Raj <sankar.raj@crystaldelta.com>
2020-05-21 23:10:30 +0530

    Removed bad option value(open-builtin)

--- test_utils/file_helpers.py
+++ test_utils/file_helpers.py
@@ -7,8 +7,6 @@ from __future__ import absolute_import, unicode_literals
 import six
 import unicodecsv
 
-
-# pylint: disable=open-builtin
 class MakeCsvStreamContextManager:
     """
     Context manager that creates a temporary csv file.
