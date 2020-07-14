4a296943e1237c7b72c8d19c0a10b85117a6327d
Brittney Exline <bexline@edx.org>
2020-07-14 13:38:06 -0600

    ENT-3139 New viewset/endpoint to create PendingEnterpriseCustomerUser records

--- enterprise/api/v1/views.py
+++ enterprise/api/v1/views.py
@@ -12,7 +12,7 @@ from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthenticat
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import detail_route, list_route
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
@@ -263,6 +263,50 @@ class EnterpriseCustomerUserViewSet(EnterpriseReadWriteModelViewSet):
         if self.request.method in ('GET',):
             return serializers.EnterpriseCustomerUserReadOnlySerializer
         return serializers.EnterpriseCustomerUserWriteSerializer
+
+
+class PendingEnterpriseCustomerUserViewSet(EnterpriseReadWriteModelViewSet):
+    """
+    API views for the ``pending-enterprise-learner`` API endpoint.
+    """
+
+    queryset = models.PendingEnterpriseCustomerUser.objects.all()
+    filter_backends = (filters.OrderingFilter, DjangoFilterBackend)
+    serializer_class = serializers.PendingEnterpriseCustomerUserSerializer
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
+
+    FIELDS = (
+        'enterprise_customer', 'user_email',
+    )
+    filterset_fields = FIELDS
+    ordering_fields = FIELDS
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as validation_error:
+            return_status = None
+            if 'user_email' in serializer.errors:
+                for error in serializer.errors['user_email']:
+                    # This error indicates that a PendingEnterpriseCustomerUser already exists.
+                    if error.code == 'unique':
+                        return_status = status.HTTP_204_NO_CONTENT
+                        break
+            elif 'non_field_errors' in serializer.errors:
+                for error in serializer.errors['non_field_errors']:
+                    # This error indicates that an EnterpriseCustomerUser already exists.
+                    if str(error) == 'EnterpriseCustomerUser record already exists':
+                        return_status = status.HTTP_204_NO_CONTENT
+                        break
+
+            if not return_status:
+                raise validation_error
+        else:
+            created = serializer.save()
+            return_status = status.HTTP_201_CREATED if created else status.HTTP_204_NO_CONTENT
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=return_status, headers=headers)
 
 
 class EnterpriseCustomerBrandingConfigurationViewSet(EnterpriseReadOnlyModelViewSet):
