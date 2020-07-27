438d5c183d9b214535014879543047b77895f48d
binodpant <bpant@edx.org>
2020-07-27 12:50:21 -0400

    First time minimal setup of Canvas integration channel

---
+++ integrated_channels/canvas/apps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Enterprise Integrated Channel Canvas Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class CanvasConfig(AppConfig):
+    """
+    Configuration for the Enterprise Integrated Channel Canvas Django application.
+    """
+    name = 'integrated_channels.canvas'
+    verbose_name = "Enterprise Canvas Integration"
---
+++ integrated_channels/canvas/client.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Client for connecting to Canvas.
+"""
+
+from django.apps import apps
+
+from integrated_channels.integrated_channel.client import IntegratedChannelApiClient
+
+
+class CanvasAPIClient(IntegratedChannelApiClient):
+    """
+    Client for connecting to Canvas.
+
+    Code to obtain access tokens and posting to the courses and
+    completion status endpoints.
+    """
+
+    def __init__(self, enterprise_configuration):
+        """
+        Instantiate a new client.
+
+        Args:
+            enterprise_configuration (CanvasEnterpriseCustomerConfiguration): An enterprise customers's
+            configuration model for connecting with Canvas
+        """
+        super(CanvasAPIClient, self).__init__(enterprise_configuration)
+        self.global_degreed_config = apps.get_model('degreed', 'CanvasGlobalConfiguration').current()
+        self.session = None
+        self.expires_at = None
+
+    def create_course_completion(self, user_id, payload):  # pylint: disable=unused-argument
+        pass
+
+    def delete_course_completion(self, user_id, payload):  # pylint: disable=unused-argument
+        pass
+
+    def create_content_metadata(self, serialized_data):
+        pass
+
+    def update_content_metadata(self, serialized_data):
+        pass
+
+    def delete_content_metadata(self, serialized_data):
+        pass
