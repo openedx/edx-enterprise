4e111bac825e715d7c005d02e35621314e8b8a9b
binodpant <bpant@edx.org>
2020-07-28 14:36:44 -0400

    add canvas to installed_apps for test env

--- tests/test_integrated_channels/test_canvas/test_client.py
+++ tests/test_integrated_channels/test_canvas/test_client.py
@@ -4,27 +4,23 @@ Tests for clients in integrated_channels.
 """
 
 import datetime
-import json
 import unittest
-
 import pytest
-import requests
-import responses
-from freezegun import freeze_time
+
 from six.moves.urllib.parse import urljoin  # pylint: disable=import-error
+from freezegun import freeze_time
 
 from django.utils import timezone
 
 from integrated_channels.canvas.client import CanvasAPIClient
-from integrated_channels.exceptions import ClientError
 from test_utils import canvas_factories
 
 NOW = datetime.datetime(2017, 1, 2, 3, 4, 5, tzinfo=timezone.utc)
 NOW_TIMESTAMP_FORMATTED = NOW.strftime('%F')
 
 
-@pytest.mark.django_db
 @freeze_time(NOW)
+@pytest.mark.django_db
 class TestCanvasApiClient(unittest.TestCase):
     """
     Test Canvas API client methods.
@@ -42,7 +38,7 @@ class TestCanvasApiClient(unittest.TestCase):
         self.company_id = "company_id"
         self.access_token = "access_token"
         self.expected_token_response_body = {
-            "expires_in": self.expires_in,
+            "expires_in": "",
             "access_token": self.access_token
         }
         canvas_factories.CanvasGlobalConfigurationFactory(
@@ -56,16 +52,8 @@ class TestCanvasApiClient(unittest.TestCase):
             canvas_base_url=self.url_base,
         )
 
-    @responses.activate
-    def test_oauth_with_non_json_response(self):
+    def test_global_canvas_config_is_set(self):
         """ Test  _get_oauth_access_token with non json type response"""
-        with pytest.raises(requests.RequestException):
-            responses.add(
-                responses.POST,
-                self.oauth_url,
-            )
-            client = CanvasAPIClient(self.enterprise_config)
-            client._get_oauth_access_token(  # pylint: disable=protected-access
-                self.client_id,
-                self.client_secret,
-            )
+        canvas_api_client = CanvasAPIClient(self.enterprise_config)
+        expected_config = {}
+        assert canvas_api_client.global_canvas_config == expected_config
