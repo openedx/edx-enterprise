6c971c7e7302fd6e7e0809f245d78e512ddff234
binodpant <bpant@edx.org>
2020-08-07 13:58:32 -0400

    Initial cut to setup transmitter with one test

---
+++ integrated_channels/canvas/transmitters/content_metadata.py
@@ -0,0 +1,25 @@
+"""
+Transmitter for Canvas content metadata
+"""
+
+from integrated_channels.integrated_channel.transmitters.content_metadata import ContentMetadataTransmitter
+
+
+class CanvasContentMetadataTransmitter(ContentMetadataTransmitter):
+    """
+    This transmitter transmits exported content metadata to Canvas.
+    """
+
+    def __init__(self, enterprise_configuration): # TODO: , client=CanvasAPIClient
+        """
+        Use the ``CanvasAPIClient`` for content metadata transmission to Canvas.
+        """
+        super(CanvasContentMetadataTransmitter, self).__init__(
+            enterprise_configuration=enterprise_configuration,
+            # TODO: client=client
+        )
+
+    def _prepare_items_for_transmission(self, channel_metadata_items):
+        return {
+            'courses': channel_metadata_items,
+        }
---
+++ tests/test_integrated_channels/test_canvas/test_transmitters/test_content_metadata.py
@@ -0,0 +1,55 @@
+"""
+Tests for the Canvas content metadata transmitter.
+"""
+
+import unittest
+from pytest import mark
+
+from test_utils import factories
+from test_utils.canvas_factories import (
+    CanvasEnterpriseCustomerConfigurationFactory,
+)
+from integrated_channels.integrated_channel.exporters.content_metadata import ContentMetadataItemExport
+from integrated_channels.canvas.transmitters.content_metadata import CanvasContentMetadataTransmitter
+
+
+@mark.django_db
+class TestCanvasContentMetadataTransmitter(unittest.TestCase):
+    """
+    Tests for the class ``CanvasContentMetadataTransmitter``.
+    """
+
+    def setUp(self):
+        super(TestCanvasContentMetadataTransmitter, self).setUp()
+        enterprise_customer = factories.EnterpriseCustomerFactory(name='Quokka Search Party')
+        self.enterprise_config = CanvasEnterpriseCustomerConfigurationFactory()
+
+    def test_prepare_items_for_transmission(self):
+        transmitter = CanvasContentMetadataTransmitter(self.enterprise_config)
+        channel_metadata_items =[ {'field': 'value'} ]
+        expected_items = {
+            'courses': channel_metadata_items,
+        }
+        assert transmitter._prepare_items_for_transmission(channel_metadata_items) == expected_items
+
+
+    @mark.skip(reason="not ready yet")
+    def test_transmit_create_metadata(self):
+        """
+        Test creation of content metadata during transmission.
+        """
+
+        content_id = 'course:DemoX'
+
+        transmitter = CanvasContentMetadataTransmitter(self.enterprise_config)
+        transmitter.transmit({
+            content_id: ContentMetadataItemExport(
+                {'key': content_id, 'content_type': 'course'},
+                {'courseID': content_id}
+            )
+        })
+        assert not ContentMetadataItemTransmission.objects.filter(
+            enterprise_customer=self.enterprise_config.enterprise_customer,
+            integrated_channel_code=self.enterprise_config.channel_code(),
+            content_id=content_id,
+        ).exists()
