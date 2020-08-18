efbe29908a9023a7b16bfcfcb7ecbe62af5b8113
binodpant <bpant@edx.org>
2020-08-18 14:06:35 -0400

    lint

--- tests/test_integrated_channels/test_canvas/test_transmitters/test_content_metadata.py
+++ tests/test_integrated_channels/test_canvas/test_transmitters/test_content_metadata.py
@@ -9,7 +9,6 @@ from test_utils import factories
 from test_utils.factories import (
     CanvasEnterpriseCustomerConfigurationFactory,
 )
-from integrated_channels.integrated_channel.exporters.content_metadata import ContentMetadataItemExport
 from integrated_channels.canvas.transmitters.content_metadata import CanvasContentMetadataTransmitter
 
 
@@ -21,12 +20,11 @@ class TestCanvasContentMetadataTransmitter(unittest.TestCase):
 
     def setUp(self):
         super(TestCanvasContentMetadataTransmitter, self).setUp()
-        enterprise_customer = factories.EnterpriseCustomerFactory(name='Quokka Search Party')
         self.enterprise_config = CanvasEnterpriseCustomerConfigurationFactory()
 
     def test_prepare_items_for_transmission(self):
         transmitter = CanvasContentMetadataTransmitter(self.enterprise_config)
-        channel_metadata_items =[ {'field': 'value'} ]
+        channel_metadata_items = [{'field': 'value'}]
         expected_items = {
             'course': channel_metadata_items[0],
         }
