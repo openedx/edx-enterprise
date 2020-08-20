496dfb5889115688694aa377291dcefdca6add5b
Talia Rhodes <trhodes@edx.org>
2020-08-20 16:56:14 -0400

    fix duplication logic.

--- integrated_channels/moodle/transmitters/content_metadata.py
+++ integrated_channels/moodle/transmitters/content_metadata.py
@@ -22,11 +22,9 @@ class MoodleContentMetadataTransmitter(ContentMetadataTransmitter):
         )
 
     def _prepare_items_for_transmission(self, channel_metadata_items):
-        course_list = []
         items = {}
         for index, item in enumerate(channel_metadata_items):
             for key in item:
                 new_key = 'courses[{0}][{1}]'.format(index, key)
                 items[new_key] = item[key]
-            course_list.append(items)
-        return course_list
+        return items
