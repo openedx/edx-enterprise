052ea6f883121250c79cac795aa73b2c8660d358
Talia Rhodes <trhodes@edx.org>
2020-08-25 16:08:22 -0400

    content metadata update/deletes

--- integrated_channels/moodle/client.py
+++ integrated_channels/moodle/client.py
@@ -3,9 +3,12 @@
 Client for connecting to Moodle.
 """
 
+import json
 import requests
 
 from django.apps import apps
+from django.utils.http import urlencode
+from urllib.parse import urlencode
 
 from integrated_channels.integrated_channel.client import IntegratedChannelApiClient
 
@@ -40,17 +43,37 @@ class MoodleAPIClient(IntegratedChannelApiClient):
     def delete_course_completion(self, user_id, payload):  # pylint: disable=unused-argument
         pass
 
+    def _get_course_id(self, key):
+        """
+        Gets the Moodle course id (because we cannot update/delete without it).
+        """
+        params = {
+            'wstoken': self.enterprise_configuration.api_token,
+            'wsfunction': 'core_course_get_courses_by_field',
+            'field': 'shortname',
+            'value': key,
+            'moodlewsrestformat': 'json'
+        }
+        response = requests.get(
+            self.enterprise_configuration.moodle_base_url,
+            params=params
+        )
+
+        return json.loads(response.text)['courses'][0]['id']
+
     def create_content_metadata(self, serialized_data):
-        # The below assumes the data is dict/object.
-        # Format should look like:
-        # {
-        #   courses[0][shortname]: 'value',
-        #   courses[0][fullname]: 'value',
-        #   [...]
-        #   courses[1][shortname]: 'value',
-        #   courses[1][fullname]: 'value',
-        #   [...]
-        # }
+        """
+        The below assumes the data is dict/object.
+        Format should look like:
+        {
+          courses[0][shortname]: 'value',
+          courses[0][fullname]: 'value',
+          [...]
+          courses[1][shortname]: 'value',
+          courses[1][fullname]: 'value',
+          [...]
+        }
+        """
 
         serialized_data['wstoken'] = self.enterprise_configuration.api_token
         serialized_data['wsfunction'] = 'core_course_create_courses'
@@ -63,13 +86,33 @@ class MoodleAPIClient(IntegratedChannelApiClient):
 
 
     def update_content_metadata(self, serialized_data):
-        # core_course_get_courses_by_field (idnumber or shortname field as our unique match)
-        # Get course id from above call first. Then hit update function based on that id.
-        # May have to get and modify data in a loop here. Actual update call can be bulk though
-        # core_course_update_courses
-        pass
+        for key in list(serialized_data):
+            if 'shortname' in key:
+                moodle_course_id = self._get_course_id(serialized_data[key])
+                serialized_data[key.replace('shortname', 'id')] = moodle_course_id
+        serialized_data['wstoken'] = self.enterprise_configuration.api_token
+        serialized_data['wsfunction'] = 'core_course_update_courses'
+        response = requests.post(
+            self.enterprise_configuration.moodle_base_url,
+            params=serialized_data
+        )
+        return response.status_code, response.text
 
     def delete_content_metadata(self, serialized_data):
-        # core_course_get_courses_by_field (idnumber or shortname field as our unique match)
-        # Delete by core_course_delete_courses
-        pass
+        course_ids_to_delete = []
+        for key in list(serialized_data):
+            if 'shortname' in key:
+                moodle_course_id = self._get_course_id(serialized_data[key])
+                course_ids_to_delete.append(('courseids[]', moodle_course_id))
+        params = {
+            'wstoken': self.enterprise_configuration.api_token,
+            'wsfunction': 'core_course_delete_courses',
+            'moodlewsrestformat': 'json',
+        }
+        url = self.enterprise_configuration.moodle_base_url + \
+            '?{}'.format(urlencode(course_ids_to_delete))
+        response = requests.post(
+            url,
+            params=params
+        )
+        return response.status_code, response.text
