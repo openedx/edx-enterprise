ecc5e3c8dee2ca1f1bcd5d2861ace31c776591b8
Talia <trhodes@edx.org>
2020-09-24 11:39:41 -0400

    pull platform sync migration test to enterprise.

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -3,68 +3,19 @@
 Tests for migrations, especially potentially risky data migrations.
 """
 
-from django.db import connection
-from django.db.migrations.executor import MigrationExecutor
-from django.test.testcases import TransactionTestCase
+from django.core.management import call_command
+from django.test.testcases import TestCase
+from django.test.utils import override_settings
+from django.utils.six import StringIO
 
 
-class MigrationTestCase(TransactionTestCase):
+class MigrationTests(TestCase):
     """
-    A base test case class for migration test cases.
+    Runs migration tests using Django Command interface.
     """
-
-    migrate_origin = None
-    migrate_dest = None
-    model = None
-
-    def setUp(self):
-        super(MigrationTestCase, self).setUp()
-        self.executor = MigrationExecutor(connection)
-        self.executor.migrate(self.migrate_origin)
-
-    def migrate_to_origin(self):
-        """
-        Performs the migration to the designated origin.
-
-        This only really does anything if you have migrated forward
-        in some way or no migrations were performed at all.
-        """
-        self.executor.loader.build_graph()
-        self.executor.migrate(self.migrate_origin)
-
-    def migrate_to_dest(self):
-        """
-        Performs the migration to the designated destination.
-        """
-        self.executor.loader.build_graph()
-        self.executor.migrate(self.migrate_dest)
-
-    def migrate_to_dest_then_origin(self):
-        """
-        Migrates to the destination and back to the origin.
-
-        Can be used as a shortcut to test a forward- and backward-migration in one-go.
-        """
-        self.migrate_to_dest()
-        self.migrate_to_origin()
-
-    @property
-    def old_apps(self):
-        """
-        Returns the app in its original state -- the one that'd exist before migration.
-        """
-        return self.executor.loader.project_state(self.migrate_origin).apps
-
-    @property
-    def new_apps(self):
-        """
-        Returns the app in a future state -- the one that'd exist after migration.
-        """
-        return self.executor.loader.project_state(self.migrate_dest).apps
-
-    @property
-    def model_label(self):
-        """
-        Returns the label of the ``model``.
-        """
-        return self.model._meta.get_field('name')
+    @override_settings(MIGRATION_MODULES={})
+    def test_migrations_are_in_sync(self):
+        out = StringIO()
+        call_command("makemigrations", dry_run=True, verbosity=3, stdout=out)
+        output = out.getvalue()
+        self.assertIn("No changes detected", output)
