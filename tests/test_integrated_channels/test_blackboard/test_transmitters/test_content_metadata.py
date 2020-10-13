efdebcfce95000e7eb1a926e6689f656a732c959
binodpant <bpant@edx.org>
2020-10-13 13:21:03 -0400

    transmitter test

---
+++ tests/test_integrated_channels/test_blackboard/test_transmitters/test_content_metadata.py
@@ -0,0 +1,31 @@
+"""
+Tests for the Blackboard content metadata transmitter.
+"""
+
+import unittest
+
+from pytest import mark
+
+from integrated_channels.blackboard.transmitters.content_metadata import BlackboardContentMetadataTransmitter
+from test_utils.factories import BlackboardEnterpriseCustomerConfigurationFactory
+
+
+@mark.django_db
+class TestBlackboardContentMetadataTransmitter(unittest.TestCase):
+    """
+    Tests for the class ``BlackboardContentMetadataTransmitter``.
+    """
+
+    def setUp(self):
+        super(TestBlackboardContentMetadataTransmitter, self).setUp()
+        self.enterprise_config = BlackboardEnterpriseCustomerConfigurationFactory()
+
+    def test_prepare_items_for_transmission(self):
+        transmitter = BlackboardContentMetadataTransmitter(self.enterprise_config)
+        channel_metadata_items = [{'field': 'value'}]
+        expected_items = channel_metadata_items[0]
+
+        # pylint: disable=protected-access
+        assert transmitter._prepare_items_for_transmission(
+            channel_metadata_items
+        ) == expected_items
