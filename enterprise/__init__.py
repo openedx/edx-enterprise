a17e4cbf88f39ee596b6d13d343959019fe25183
binodpant <bpant@edx.org>
2020-10-15 09:20:54 -0400

    init py fix

--- enterprise/__init__.py
+++ enterprise/__init__.py
@@ -2,10 +2,6 @@
 Your project description goes here.
 """
 
-<<<<<<< HEAD
 __version__ = "3.8.39"
-=======
-__version__ = "3.8.37"
->>>>>>> version
 
 default_app_config = "enterprise.apps.EnterpriseConfig"  # pylint: disable=invalid-name
