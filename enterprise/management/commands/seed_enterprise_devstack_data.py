1db5170d53b1117ddef5e29a60f5f3ccb6402b4c
Kellie Selinka <>
2020-12-21 21:40:03 -0500

    fix seed_enterprise_devstack_data script by setting a default for enable_portal_lms_configurations_screen since the db column doesn't have one

--- enterprise/management/commands/seed_enterprise_devstack_data.py
+++ enterprise/management/commands/seed_enterprise_devstack_data.py
@@ -89,6 +89,7 @@ class Command(BaseCommand):
                 'enable_portal_reporting_config_screen': True,
                 'enable_portal_saml_configuration_screen': True,
                 'enable_portal_subscription_management_screen': True,
+                'enable_portal_lms_configurations_screen': True,
             },
         )
         return enterprise_customer
