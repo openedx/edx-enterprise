3c05c0eab379d94be3cc5b508b3c7c1dbc2e803f
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-01-06 13:55:23 +0500

    Upgrading celery to 5.0.4

--- setup.py
+++ setup.py
@@ -121,7 +121,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.8",
     ],
 )
