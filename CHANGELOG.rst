5f7487fec18aeae9a88d909b92a4ab80ba24de82
Adam Stankiewicz <astankiewicz@edx.org>
2021-01-12 16:24:53 -0500

    fix: always return "completed" course run status if learner has a passing certificate (#1111)
    * fix: return completed course run status before 30 day cert window
    
    * changelog entry
    
    * quality
    
    * fix failing tests
    
    * fix quality
    
    * remove docstirng

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -14,7 +14,12 @@ Change Log
 .. There should always be an "Unreleased" section for changes pending release.
 
 Unreleased
------------
+----------
+
+[3.17.4]
+--------
+
+* Ensure enterprise course enrollments return valid course run statuses such that when a learner earns a passing certificate, the ``enterprise_course_enrollments`` API endpoint deems the course is complete even though the course itself may not have ended yet per the configured dates.
 
 [3.17.4]
 --------
--- enterprise_learner_portal/utils.py
+++ enterprise_learner_portal/utils.py
@@ -3,10 +3,6 @@
 enterprise_learner_portal utils.
 """
 
-from datetime import datetime, timedelta
-
-from pytz import UTC
-
 
 class CourseRunProgressStatuses:
     """
@@ -21,45 +17,31 @@ class CourseRunProgressStatuses:
 
 def get_course_run_status(course_overview, certificate_info, enterprise_enrollment):
     """
-    Get the progress status of a course run, given the state of a user's certificate in the course.
+    Get the status of a course run, given the state of a user's certificate in the course.
 
-    In the case of self-paced course runs, the run is considered completed when either the course run has ended
-    OR the user has earned a passing certificate 30 days ago or longer.
+    A run is considered "complete" when either the course run has ended OR the user has earned a
+    passing certificate.
 
     Arguments:
         course_overview (CourseOverview): the overview for the course run
-        certificate_info: A dict containing the following keys:
+        certificate_info: A dict containing the following key:
             ``is_passing``: whether the  user has a passing certificate in the course run
-            ``created``: the date the certificate was created
 
     Returns:
         status: one of (
+            CourseRunProgressStatuses.SAVED_FOR_LATER,
             CourseRunProgressStatuses.COMPLETE,
             CourseRunProgressStatuses.IN_PROGRESS,
             CourseRunProgressStatuses.UPCOMING,
         )
-        None if pacing type is not matched
     """
-    is_certificate_passing = certificate_info.get('is_passing', False)
-    certificate_creation_date = certificate_info.get('created', datetime.max)
-
     if enterprise_enrollment and enterprise_enrollment.saved_for_later:
         return CourseRunProgressStatuses.SAVED_FOR_LATER
 
-    if course_overview['pacing'] == 'instructor':
-        if course_overview['has_ended']:
-            return CourseRunProgressStatuses.COMPLETED
-        if course_overview['has_started']:
-            return CourseRunProgressStatuses.IN_PROGRESS
-        return CourseRunProgressStatuses.UPCOMING
+    is_certificate_passing = certificate_info.get('is_passing', False)
 
-    if course_overview['pacing'] == 'self':
-        thirty_days_ago = datetime.now(UTC) - timedelta(30)
-        certificate_completed = is_certificate_passing and (certificate_creation_date <= thirty_days_ago)
-        if course_overview['has_ended'] or certificate_completed:
-            return CourseRunProgressStatuses.COMPLETED
-        if course_overview['has_started']:
-            return CourseRunProgressStatuses.IN_PROGRESS
-        return CourseRunProgressStatuses.UPCOMING
-
-    return None
+    if course_overview['has_ended'] or is_certificate_passing:
+        return CourseRunProgressStatuses.COMPLETED
+    if course_overview['has_started']:
+        return CourseRunProgressStatuses.IN_PROGRESS
+    return CourseRunProgressStatuses.UPCOMING
