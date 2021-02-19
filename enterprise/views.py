613b0ead53fc743de4799f2d65fe836229edb894
Adrienne Bolger <abolger@edx.org>
2021-02-19 14:14:22 -0500

    Fixed line too long error.

--- enterprise/views.py
+++ enterprise/views.py
@@ -577,7 +577,9 @@ class GrantDataSharingPermissions(View):
 
         if request.GET.get('left_sidebar_text_override') is not None:
             # Allows sidebar text to be overridden by calling API
-            context_data.update({'text_override_available': True, 'left_sidebar_text': request.GET.get('left_sidebar_text_override')})
+            context_data.update({'text_override_available': True,
+                                 'left_sidebar_text': request.GET.get('left_sidebar_text_override')
+                                 })
 
         return context_data
 
