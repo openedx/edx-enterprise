65ca598401bbf387592288600a76ec5bf473a73f
HammadAhmadWaqas <hammadahmadwaqas1@gmail.com>
2021-05-10 14:26:08 +0500

    added support for enrollment_before and  --no-commit in email_drip_for_missing_dsc_records command

--- enterprise/management/commands/email_drip_for_missing_dsc_records.py
+++ enterprise/management/commands/email_drip_for_missing_dsc_records.py
@@ -84,29 +84,87 @@ class Command(BaseCommand):
         )
         return dsc_url, course_title
 
+    def get_enterprise_course_enrollments(self, options):
+        """
+        Get EnterpriseCourseEnrollment records according to the options
+        """
+        enrollment_before = options['enrollment_before']
+        enterprise_course_enrollments = EnterpriseCourseEnrollment.objects.select_related(
+            'enterprise_customer_user'
+        )
+        if enrollment_before:
+            enterprise_course_enrollments = enterprise_course_enrollments.filter(created__date__lt=enrollment_before)
+        else:
+            past_date = date.today() - timedelta(days=PAST_NUM_DAYS)
+            enterprise_course_enrollments = enterprise_course_enrollments.filter(created__date=past_date)
+
+        return enterprise_course_enrollments
+
+    def emit_event(self, ec_user,  course_id, enterprise_customer, greeting_name):
+        """
+         Emit the Segment event which will be used by Braze to send the email
+        """
+        dsc_url, course_title = self._get_course_properties(ec_user.user, course_id, enterprise_customer)
+        utils.track_event(ec_user.user_id, 'edx.bi.user.consent.absent', {
+            'course': course_id,
+            'username': ec_user.username,
+            'enterprise_name': enterprise_customer.name,
+            'enterprise_uuid': str(enterprise_customer.uuid),
+            'dsc_url': dsc_url,
+            'course_title': course_title,
+            'user_email': ec_user.user_email,
+            'greeting_name': greeting_name
+        })
+        LOGGER.info(
+            '[Absent DSC Email] Segment event fired for missing data sharing consent. '
+            'User: [%s], Course: [%s], Enterprise: [%s], DSC URL: [%s]',
+            ec_user.username,
+            course_id,
+            str(enterprise_customer.uuid),
+            dsc_url
+        )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--no-commit',
+            action='store_true',
+            dest='no_commit',
+            default=False,
+            help='Dry Run, print log messages without committing anything.',
+        )
+        parser.add_argument(
+            '--enrollment-before',
+            action='store',
+            dest='enrollment_before',
+            default=None,
+            type=datetime.date.fromisoformat,
+            help='Specifies the date (format YYYY-MM-DD). Enrollments created before this date will receive DSC emails.'
+        )
+
     def handle(self, *args, **options):
         """
         Management command for sending an email to learners with a missing DSC. Designed to run daily.
 
         Example usage:
            $ ./manage.py email_drip_for_missing_dsc_records
+           $ ./manage.py email_drip_for_missing_dsc_records  --no-commit
+           $ ./manage.py email_drip_for_missing_dsc_records  --enrollment-before 2021-05-06 --no-commit
+           $ ./manage.py email_drip_for_missing_dsc_records  --enrollment-before 2021-05-06
         """
-        past_date = date.today() - timedelta(days=PAST_NUM_DAYS)
-        enterprise_course_enrollments = EnterpriseCourseEnrollment.objects.select_related(
-            'enterprise_customer_user'
-        ).filter(created__date=past_date)
+        should_commit = not options['no_commit']
 
+        email_sent_records = []
+
+        enterprise_course_enrollments = self.get_enterprise_course_enrollments(options)
         for enterprise_enrollment in enterprise_course_enrollments:
-            user = enterprise_enrollment.enterprise_customer_user.user
-            username = enterprise_enrollment.enterprise_customer_user.username
-            user_id = enterprise_enrollment.enterprise_customer_user.user_id
-            user_email = enterprise_enrollment.enterprise_customer_user.user_email
+            ec_user = enterprise_enrollment.enterprise_customer_user
+            username = ec_user.username
+            user_email = ec_user.user_email
             greeting_name = user_email
-            if hasattr(user, 'first_name') and user.first_name:
-                greeting_name = user.first_name
+            if hasattr(ec_user, 'first_name') and ec_user.first_name:
+                greeting_name = ec_user.first_name
             course_id = enterprise_enrollment.course_id
-            enterprise_customer = enterprise_enrollment.enterprise_customer_user.enterprise_customer
-            dsc_url, course_title = self._get_course_properties(user, course_id, enterprise_customer)
+            enterprise_customer = ec_user.enterprise_customer
             consent = DataSharingConsent.objects.proxied_get(
                 username=username,
                 course_id=course_id,
@@ -114,21 +172,17 @@ class Command(BaseCommand):
             )
             # Emit the Segment event which will be used by Braze to send the email
             if isinstance(consent, ProxyDataSharingConsent):
-                utils.track_event(user_id, 'edx.bi.user.consent.absent', {
-                    'course': course_id,
-                    'username': username,
-                    'enterprise_name': enterprise_customer.name,
-                    'enterprise_uuid': str(enterprise_customer.uuid),
-                    'dsc_url': dsc_url,
-                    'course_title': course_title,
-                    'user_email': user_email,
-                    'greeting_name': greeting_name
-                })
-                LOGGER.info(
-                    '[Absent DSC Email] Segment event fired for missing data sharing consent. '
-                    'User: [%s], Course: [%s], Enterprise: [%s], DSC URL: [%s]',
-                    username,
-                    course_id,
-                    str(enterprise_customer.uuid),
-                    dsc_url
+                if should_commit:
+                    self.emit_event(ec_user,  course_id, enterprise_customer, greeting_name)
+                email_sent_records.append(
+                    f'User: {username}, Course: {course_id}, Enterprise: {enterprise_customer.uuid}'
                 )
+
+        LOGGER.info(
+            '[Absent DSC Email] Email sent for [%s] enrollments out of [%s] enrollments. DSC records sent to: [%s]'
+            'User: [%s], Course: [%s], Enterprise: [%s], DSC URL: [%s]',
+            len(email_sent_records),
+            enterprise_course_enrollments.count(),
+            email_sent_records
+        )
+
