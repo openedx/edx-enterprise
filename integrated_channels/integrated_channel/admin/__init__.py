6e3c89f7992b1431cf11855fbec309121b44f773
Matt Drayer <mattdrayer@hotmail.com>
2021-05-28 17:17:38 -0400

    Remove hardcoded workflow constraints in order to utilize app-permissions framework

--- integrated_channels/integrated_channel/admin/__init__.py
+++ integrated_channels/integrated_channel/admin/__init__.py
@@ -14,9 +14,3 @@ class ContentMetadataItemTransmissionAdmin(admin.ModelAdmin):
     """
     list_display = ('enterprise_customer', 'integrated_channel_code', 'content_id', 'channel_metadata')
     search_fields = ('enterprise_customer', 'integrated_channel_code', 'content_id')
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
