b35bf846d849f78f08c0c5899428937bae84a2a7
binodpant <bpant@edx.org>
2021-07-23 11:38:50 -0400

    feat: fix mock for tracker

--- tests/test_enterprise/api/test_views.py
+++ tests/test_enterprise/api/test_views.py
@@ -3426,7 +3426,7 @@ class TestBulkEnrollment(BaseTestEnterpriseAPIViews):
     )
     @ddt.unpack
     @mock.patch('enterprise.api.v1.views.get_best_mode_from_course_key')
-    @mock.patch('enterprise.utils.track_enrollment')
+    @mock.patch('enterprise.api.v1.views.track_enrollment')
     # pylint: disable=unused-argument
     def test_bulk_enrollment_in_bulk_courses_pending_licenses(
         self,
