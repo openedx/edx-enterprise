fec02bc01ccaee1b0a748f40870138650aa214c1
John Nagro <jnagro@edx.org>
2021-09-21 16:53:30 -0400

    fix: Moodle: Integrated channel experiences "unknown error" scenario
    PR feedback
    
    ENT-4902

--- integrated_channels/moodle/client.py
+++ integrated_channels/moodle/client.py
@@ -36,14 +36,10 @@ def moodle_request_wrapper(method):
             # This cannot be converted to JSON thus the above fails miserably.
             # The above can fail with different errors depending on the format of the returned page.
             # Moodle of course does not tell us what is wrong in any part of this HTML.
-            log_format_keys = {'method': method.__name__,
-                               'code': response.status_code,
-                               'enterprise_configuration_id': self.enterprise_configuration.id}
-            log_format_string = '''\
-                                Moodle API task "{method}" for enterprise_configuration "{enterprise_configuration_id}"
-                                failed due to unknown error with code "{code}".
-                                '''
-            raise ClientError(log_format_string.format(**log_format_keys), response.status_code) from error
+            log_msg = (f'Moodle API task "{method.__name__}" '
+                       f'for enterprise_customer_uuid "{self.enterprise_configuration.enterprise_customer.uuid}" '
+                       f'failed due to unknown error with code "{response.status_code}".')
+            raise ClientError(log_msg, response.status_code) from error
         if isinstance(body, list):
             # On course creation (and ONLY course creation) success,
             # Moodle returns a list of JSON objects, because of course it does.
