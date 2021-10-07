89d4ded4802bf53549a00c22289418a2b920eca7
Alexander Sheehan <asheehan@edx.org>
2021-10-06 20:22:37 -0400

    bug: forcing catalog content retrieval on all customer catalogs

--- tests/test_integrated_channels/test_integrated_channel/test_exporters/test_content_metadata.py
+++ tests/test_integrated_channels/test_integrated_channel/test_exporters/test_content_metadata.py
@@ -69,43 +69,22 @@ class TestContentMetadataExporter(unittest.TestCase, EnterpriseMockMixin):
         """
         ``ContentMetadataExporter``'s ``export`` produces a JSON dump of the course data.
         """
-        mock_ent_catalog_api.return_value.get_content_metadata.return_value = get_fake_content_metadata()
-        mock_ent_catalog_api.return_value.get_enterprise_catalog.return_value = self.fake_catalog
         exporter = ContentMetadataExporter('fake-user', self.config)
         exporter.export()
-        assert mock_ent_catalog_api.called
-        assert mock_ent_catalog_api.return_value.get_enterprise_catalog.call_args[0][0] == \
-            self.config.enterprise_customer.enterprise_customer_catalogs.first().uuid
+        assert not mock_ent_catalog_api.return_value.get_enterprise_catalog.called
+        assert mock_ent_catalog_api.return_value.get_content_metadata.called
         mock_ent_catalog_api.return_value.get_content_metadata.assert_called_with(
             self.config.enterprise_customer,
             [self.config.enterprise_customer.enterprise_customer_catalogs.first()]
         )
 
-        mock_ent_catalog_api.return_value.get_content_metadata.return_value = get_fake_content_metadata()
-        enterprise_catalog_data = {
-            'uuid': str(self.enterprise_customer_catalog.uuid),
-            'title': self.enterprise_customer_catalog.title,
-            'enterprise_customer': str(self.enterprise_customer_catalog.enterprise_customer.uuid),
-            'catalog_query_uuid': str(self.enterprise_customer_catalog.enterprise_catalog_query.uuid),
-            'content_last_modified': str(self.enterprise_customer_catalog.enterprise_catalog_query.modified),
-            'catalog_modified': str(self.enterprise_customer_catalog.modified)
-        }
-        # catalog_modified_date = max(
-        #     enterprise_catalog_data['content_last_modified'], enterprise_catalog_data['catalog_modified']
-        # )
-        mock_ent_catalog_api.return_value.get_enterprise_catalog.return_value = enterprise_catalog_data
         self.config.catalogs_to_transmit = str(self.enterprise_customer_catalog.uuid)
         self.config.save()
         exporter.export()
-        assert mock_ent_catalog_api.return_value.get_enterprise_catalog.call_count == 2
+
         assert mock_ent_catalog_api.return_value.get_content_metadata.call_count == 2
-        # 'catalogs_to_transmit' argument has valid uuid so only that catalog will be transmitted.
-        assert mock_ent_catalog_api.return_value.get_enterprise_catalog.call_args[0][0] == \
-            self.config.customer_catalogs_to_transmit.first().uuid
-        mock_ent_catalog_api.return_value.get_content_metadata.assert_called_with(
-            self.config.enterprise_customer,
-            [self.enterprise_customer_catalog]
-        )
+        assert mock_ent_catalog_api.return_value.get_content_metadata.call_args[0][1][0].uuid == \
+               self.enterprise_customer_catalog.uuid
 
     @mock.patch('integrated_channels.integrated_channel.exporters.content_metadata.EnterpriseCatalogApiClient')
     def test_content_exporter_bad_data_transform_mapping(self, mock_api_client):
@@ -124,32 +103,33 @@ class TestContentMetadataExporter(unittest.TestCase, EnterpriseMockMixin):
             )
             assert expected_message in log_capture.records[0].getMessage()
 
-    @mock.patch('integrated_channels.integrated_channel.exporters.content_metadata.EnterpriseCatalogApiClient')
-    def test_export_fetches_content_only_when_update_needed(self, mock_ent_catalog_api):
-        """
-        Test that when a ContentMetadataItemTransmission exists with a `catalog_last_changed` field that comes after the
-        last modified time of content's associated catalog, we don't fetch the catalog's metadata.
-        """
-        # Generate a past transmission item that will indicate no updated needed
-        past_content_transmission = factories.ContentMetadataItemTransmissionFactory(
-            enterprise_customer=self.config.enterprise_customer,
-            integrated_channel_code=self.config.channel_code(),
-            content_last_changed='2021-07-16T15:11:10.521611Z',
-            enterprise_customer_catalog_uuid=self.enterprise_customer_catalog.uuid
-        )
-        mock_ent_catalog_api.return_value.get_content_metadata.return_value = get_fake_content_metadata()
-        mock_ent_catalog_api.return_value.get_enterprise_catalog.return_value = self.fake_catalog
-        exporter = ContentMetadataExporter('fake-user', self.config)
-        payload = exporter.export()
-
-        # Even though no metadata was fetched, in order to report a `No updates needed` to the transmitter, the exporter
-        # will still return a payload. However, this payload will contain exactly what's kept in the content item audit
-        # record.
-        assert len(payload) == 1
-        for transmission in payload.values():
-            assert transmission.content_id == past_content_transmission.content_id
-            assert transmission.channel_metadata == past_content_transmission.channel_metadata
-
-        # We should make a call for the enterprise catalog, but no call for the metadata because no update needed
-        assert mock_ent_catalog_api.return_value.get_content_metadata.call_count == 0
-        assert mock_ent_catalog_api.return_value.get_enterprise_catalog.call_count == 1
+    #  NOTE: Catalog needs updating requests are forced overwritten to True as of 10-06-2021
+    # @mock.patch('integrated_channels.integrated_channel.exporters.content_metadata.EnterpriseCatalogApiClient')
+    # def test_export_fetches_content_only_when_update_needed(self, mock_ent_catalog_api):
+    #     """
+    #     Test that when a ContentMetadataItemTransmission exists with a `catalog_last_changed` field that comes after
+    #     the last modified time of content's associated catalog, we don't fetch the catalog's metadata.
+    #     """
+    #     # Generate a past transmission item that will indicate no updated needed
+    #     past_content_transmission = factories.ContentMetadataItemTransmissionFactory(
+    #         enterprise_customer=self.config.enterprise_customer,
+    #         integrated_channel_code=self.config.channel_code(),
+    #         content_last_changed='2021-07-16T15:11:10.521611Z',
+    #         enterprise_customer_catalog_uuid=self.enterprise_customer_catalog.uuid
+    #     )
+    #     mock_ent_catalog_api.return_value.get_content_metadata.return_value = get_fake_content_metadata()
+    #     mock_ent_catalog_api.return_value.get_enterprise_catalog.return_value = self.fake_catalog
+    #     exporter = ContentMetadataExporter('fake-user', self.config)
+    #     payload = exporter.export()
+    #
+    #     # Even though no metadata was fetched, in order to report a `No updates needed` to the transmitter, the
+    #     # exporter will still return a payload. However, this payload will contain exactly what's kept in the content
+    #     # item audit record.
+    #     assert len(payload) == 1
+    #     for transmission in payload.values():
+    #         assert transmission.content_id == past_content_transmission.content_id
+    #         assert transmission.channel_metadata == past_content_transmission.channel_metadata
+    #
+    #     # We should make a call for the enterprise catalog, but no call for the metadata because no update needed
+    #     assert mock_ent_catalog_api.return_value.get_content_metadata.call_count == 0
+    #     assert mock_ent_catalog_api.return_value.get_enterprise_catalog.call_count == 1
