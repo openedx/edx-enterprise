0f3fc7d609233582769ac774499481e2dad6933e
John Nagro <jnagro@edx.org>
2021-12-07 16:18:15 -0500

    feat: OAuth Auth link for Blackboard Admin (#1424)
    ENT-4389

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -17,6 +17,10 @@ Unreleased
 ----------
 * Nothing
 
+[3.36.4]
+--------
+feat: OAuth Auth link for Blackboard Admin
+
 [3.36.3]
 --------
 feat: Integrated channels, grade send logic only logs instead of raising when enterprise_customer_user record is inactive
--- integrated_channels/blackboard/admin/__init__.py
+++ integrated_channels/blackboard/admin/__init__.py
@@ -2,9 +2,16 @@
 Admin integration for configuring Blackboard app to communicate with Blackboard systems.
 """
 
-from django.contrib import admin
+from six.moves.urllib.parse import urljoin
 
+from django.conf import settings
+from django.contrib import admin
+from django.utils.html import format_html
+
+from enterprise.utils import get_configuration_value
 from integrated_channels.blackboard.models import BlackboardEnterpriseCustomerConfiguration
+
+LMS_OAUTH_REDIRECT_URL = urljoin(settings.LMS_ROOT_URL, '/blackboard/oauth-complete')
 
 
 @admin.register(BlackboardEnterpriseCustomerConfiguration)
@@ -22,6 +29,7 @@ class BlackboardEnterpriseCustomerConfigurationAdmin(admin.ModelAdmin):
     readonly_fields = (
         "enterprise_customer_name",
         "refresh_token",
+        "oauth_authorization_url",
     )
 
     search_fields = ("enterprise_customer_name",)
@@ -38,3 +46,19 @@ class BlackboardEnterpriseCustomerConfigurationAdmin(admin.ModelAdmin):
                 being rendered with this admin form.
         """
         return obj.enterprise_customer.name
+
+    def oauth_authorization_url(self, obj):
+        """
+        Returns: the oauth authorization url when the blackboard_base_url and client_id are available.
+
+        Args:
+            obj: The instance of BlackboardEnterpriseCustomerConfiguration
+                being rendered with this admin form.
+        """
+        if obj.blackboard_base_url and obj.client_id:
+            return format_html((f'<a href="{obj.blackboard_base_url}/learn/api/public/v1/oauth2/authorizationcode'
+                                f'?redirect_uri={LMS_OAUTH_REDIRECT_URL}&'
+                                f'scope=read%20write%20delete%20offline&response_type=code&'
+                                f'client_id={obj.client_id}&state={obj.enterprise_customer.uuid}">Authorize Link</a>'))
+        else:
+            return None
