1484e51bb8a0151c4070bb4f57b060d0a5495071
Adam Stankiewicz <agstanki@gmail.com>
2022-01-27 11:10:26 -0500

    feat: add management command to ensure 1 active ECU per user

---
+++ enterprise/management/commands/ensure_singular_active_enterprise_customer_user.py
@@ -0,0 +1,88 @@
+"""
+Django management command to ensure there is at most a single EnterpriseCustomerUser with `active=True` for each enterprise user.
+"""
+import logging
+from time import sleep
+
+from django.apps import apps
+from django.core.management import BaseCommand
+from django.db.models import Count
+
+from enterprise.utils import batch
+
+log = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    """
+    Django management command to ensure there is at most a single EnterpriseCustomerUser with `active=True` for each enterprise user.
+
+    Example usage:
+    ./manage.py lms ensure_singular_active_enterprise_customer_user
+    """
+
+    def add_arguments(self, parser):
+        """
+        Entry point for subclassed commands to add custom arguments.
+        """
+        parser.add_argument(
+            '--batch-limit',
+            action='store',
+            dest='batch_limit',
+            default=100,
+            help='Number of records in each batch.',
+            type=int,
+        )
+
+        parser.add_argument(
+            '--batch-sleep',
+            action='store',
+            dest='batch_sleep',
+            default=5,
+            help='How long to sleep between batches.',
+            type=int,
+        )
+
+    def _enterprise_customer_user_model(self):
+        EnterpriseCustomerUser = apps.get_model(
+            'enterprise',
+            'EnterpriseCustomerUser'
+        )
+        return EnterpriseCustomerUser
+
+    def _historical_enterprise_customer_user_model(self):
+        HistoricalEnterpriseCustomerUser = apps.get_model(
+            'enterprise',
+            'HistoricalEnterpriseCustomerUser'
+        )
+        return HistoricalEnterpriseCustomerUser
+
+    def _most_recent_active_ecu(self):
+        return None
+
+    def _process_user(self, user):
+        EnterpriseCustomerUser = self._enterprise_customer_user_model()
+        HistoricalEnterpriseCustomerUser = self._historical_enterprise_customer_user_model()
+        lms_user_id = user['user_id']
+        current_active_ecu_count = user['ecu_count']
+        log.info('Processing LMS User ID %s', lms_user_id)
+        most_recent_active_ecu = HistoricalEnterpriseCustomerUser.objects.filter(user_id=lms_user_id, active=True).order_by('-history_date').first()
+        ecus_for_user = EnterpriseCustomerUser.objects.filter(user_id=lms_user_id).exclude(id=most_recent_active_ecu.id)
+        for ecu in ecus_for_user:
+            ecu.active = False
+        EnterpriseCustomerUser.objects.bulk_update(ecus_for_user, ['active'])
+        log.info('Successfully updated %s of %s EnterpriseCustomerUser objects for LMS User ID %s', len(ecus_for_user), current_active_ecu_count, lms_user_id)
+
+    def handle(self, *args, **options):
+        batch_limit = options['batch_limit']
+        batch_sleep = options['batch_sleep']
+
+        EnterpriseCustomerUser = self._enterprise_customer_user_model()
+        user_ids_by_active_ecu_count = EnterpriseCustomerUser.objects.filter(active=True).values('user_id').annotate(ecu_count=Count('user_id')).order_by('-ecu_count').filter(ecu_count__gt=1)
+
+        log.info('Found %s enterprise users with mulitple active EnterpriseCustomerUser objects', len(user_ids_by_active_ecu_count))
+
+        for user_batch in batch(user_ids_by_active_ecu_count, batch_size=batch_limit):
+            for user in user_batch:
+                self._process_user(user)
+            sleep(batch_sleep)
+
---
+++ tests/test_enterprise/management/test_ensure_singular_active_enterprise_customer_user.py
@@ -0,0 +1,48 @@
+"""
+Tests for the djagno management command `ensure_singular_active_enterprise_customer_user`.
+"""
+
+import ddt
+from pytest import mark
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from enterprise.models import EnterpriseCustomerUser
+from test_utils.factories import EnterpriseCustomerFactory, EnterpriseCustomerUserFactory
+
+EXCEPTION = "DUMMY_TRACE_BACK"
+
+
+@mark.django_db
+@ddt.ddt
+class CreateEnterpriseCourseEnrollmentCommandTests(TestCase):
+    """
+    Test command `ensure_singular_active_enterprise_customer_user`.
+    """
+    command = 'ensure_singular_active_enterprise_customer_user'
+
+    def setUp(self):
+        self.lms_user_id = 1
+        self.enterprise_customer_1 = EnterpriseCustomerFactory()
+        self.enterprise_customer_2 = EnterpriseCustomerFactory()
+        self.ecu_1 = EnterpriseCustomerUserFactory(user_id=self.lms_user_id, enterprise_customer=self.enterprise_customer_1, active=True)
+        self.ecu_2 = EnterpriseCustomerUserFactory(user_id=self.lms_user_id, enterprise_customer=self.enterprise_customer_2, active=True)
+        super().setUp()
+
+    def test_bulk_query_update_only_changes_provided_query(self):
+        """
+        Test that the `ensure_singular_active_enterprise_customer_user` command will ensure only one ECU object is active for a given LMS user id.
+        """
+        assert EnterpriseCustomerUser.objects.filter(user_id=self.lms_user_id, active=True).count() == 2
+        call_command(self.command)
+        assert EnterpriseCustomerUser.objects.filter(
+          user_id=self.lms_user_id,
+          enterprise_customer=self.enterprise_customer_1,
+          active=False,
+        ).count() == 1
+        assert EnterpriseCustomerUser.objects.filter(
+          user_id=self.lms_user_id,
+          enterprise_customer=self.enterprise_customer_2,
+          active=True,
+        ).count() == 1
