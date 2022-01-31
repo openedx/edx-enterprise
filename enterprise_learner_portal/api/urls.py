bfe414a19afa17ac3655483d48009c5cf2a2ab2d
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2022-01-31 15:29:52 +0500

    fix: fixed RemovedInDjango40 warnings (#1450)

--- enterprise_learner_portal/api/urls.py
+++ enterprise_learner_portal/api/urls.py
@@ -2,8 +2,9 @@
 URL definitions for enterprise_learner_portal API endpoint.
 """
 
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
-    url(r'^v1/', include('enterprise_learner_portal.api.v1.urls'), name='v1')
+    path('v1/', include('enterprise_learner_portal.api.v1.urls'), name='v1')
 ]
