79ee9ecb3f0776b12aaf5bbca7c2daa161e2bd93
Diego Millan <diego.millan@edunext.co>
2022-03-02 10:06:46 -0500

    Changing version as required.

--- enterprise/__init__.py
+++ enterprise/__init__.py
@@ -4,6 +4,6 @@ Your project description goes here.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "3.2.14"
+__version__ = "3.2.14.post1"
 
 default_app_config = "enterprise.apps.EnterpriseConfig"  # pylint: disable=invalid-name
