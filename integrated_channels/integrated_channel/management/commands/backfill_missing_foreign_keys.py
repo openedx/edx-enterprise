7fa6171877efc198e48791de380d1a79a335b115
John Nagro <jnagro@edx.org>
2022-03-30 15:14:43 -0400

    feat: management command to backfill missing fks
    - backfill missing fks on audit records
    
    ENT-5605

---
+++ integrated_channels/integrated_channel/management/commands/backfill_missing_foreign_keys.py
@@ -0,0 +1,184 @@
+"""
+Backfill missing audit record foreign keys.
+"""
+import logging
+
+from django.apps import apps
+from django.contrib import auth
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand, CommandError
+from django.db import migrations
+from django.db.models import Max, Min
+from django.utils.translation import gettext as _
+
+from integrated_channels.integrated_channel.management.commands import IntegratedChannelCommandMixin
+from integrated_channels.blackboard.models import (
+    BlackboardEnterpriseCustomerConfiguration,
+    BlackboardLearnerAssessmentDataTransmissionAudit,
+)
+from integrated_channels.canvas.models import (
+    CanvasEnterpriseCustomerConfiguration,
+    CanvasLearnerDataTransmissionAudit,
+)
+from integrated_channels.cornerstone.models import (
+    CornerstoneEnterpriseCustomerConfiguration,
+    CornerstoneLearnerDataTransmissionAudit,
+)
+from integrated_channels.degreed.models import (
+    DegreedEnterpriseCustomerConfiguration,
+    DegreedLearnerDataTransmissionAudit,
+)
+from integrated_channels.degreed2.models import (
+    Degreed2EnterpriseCustomerConfiguration,
+    Degreed2LearnerDataTransmissionAudit,
+)
+from integrated_channels.moodle.models import (
+    MoodleEnterpriseCustomerConfiguration,
+    MoodleLearnerDataTransmissionAudit,
+)
+from integrated_channels.sap_success_factors.models import (
+    SAPSuccessFactorsEnterpriseCustomerConfiguration,
+    SapSuccessFactorsLearnerDataTransmissionAudit,
+)
+from integrated_channels.integrated_channel.models import (
+    GenericEnterpriseCustomerPluginConfiguration,
+    GenericLearnerDataTransmissionAudit,
+)
+from integrated_channels.integrated_channel.models import (
+    ContentMetadataItemTransmission
+)
+
+MODELS = {
+    'BLACKBOARD': [BlackboardEnterpriseCustomerConfiguration, BlackboardLearnerAssessmentDataTransmissionAudit],
+    'CANVAS': [CanvasEnterpriseCustomerConfiguration, CanvasLearnerDataTransmissionAudit],
+    'CSOD': [CornerstoneEnterpriseCustomerConfiguration, CornerstoneLearnerDataTransmissionAudit],
+    'DEGREED': [DegreedEnterpriseCustomerConfiguration, DegreedLearnerDataTransmissionAudit],
+    'DEGREED2': [Degreed2EnterpriseCustomerConfiguration, Degreed2LearnerDataTransmissionAudit],
+    'GENERIC': [GenericEnterpriseCustomerPluginConfiguration, GenericLearnerDataTransmissionAudit],
+    'MOODLE': [MoodleEnterpriseCustomerConfiguration, MoodleLearnerDataTransmissionAudit],
+    'SAP': [SAPSuccessFactorsEnterpriseCustomerConfiguration, SapSuccessFactorsLearnerDataTransmissionAudit],
+}
+
+LOGGER = logging.getLogger(__name__)
+
+User = auth.get_user_model()
+
+class Command(IntegratedChannelCommandMixin, BaseCommand):
+    """
+    Management command which backfills missing audit record foreign keys.
+    """
+    help = _('''
+    Backfill missing audit record foreign keys.
+    ''')
+    stealth_options = ('enterprise_customer_slug', 'user1', 'user2')
+
+    def add_arguments(self, parser):
+        """
+        Add required --api_user argument to the parser.
+        """
+        parser.add_argument(
+            '--api_user',
+            dest='api_user',
+            required=True,
+            metavar='LMS_API_USERNAME',
+            help=_('Username of a user authorized to fetch grades from the LMS API.'),
+        )
+        super().add_arguments(parser)
+
+    def enterprise_course_enrollment_model():
+        """
+        Returns the ``EnterpriseCourseEnrollment`` class.
+        """
+        return apps.get_model('enterprise', 'EnterpriseCourseEnrollment')
+
+
+    def enterprise_customer_user_model():
+        """
+        Returns the ``EnterpriseCustomerUser`` class.
+        """
+        return apps.get_model('enterprise', 'EnterpriseCustomerUser')
+
+
+    def enterprise_customer_model():
+        """
+        Returns the ``EnterpriseCustomer`` class.
+        """
+        return apps.get_model('enterprise', 'EnterpriseCustomer')
+
+
+    def batch_by_pk(ModelClass, batch_size=100):
+        """
+        using limit/offset does a lot of table scanning to reach higher offsets
+        this scanning can be slow on very large tables
+        if you order by pk, you can use the pk as a pivot rather than offset
+        this utilizes the index, which is faster than scanning to reach offset
+        """
+        qs = ModelClass.objects.order_by('pk')[:batch_size]
+        while qs.exists():
+            yield qs
+            # qs.last() doesn't work here because we've already sliced
+            # loop through so we eventually grab the last one
+            for item in qs:
+                start_pk = item.pk
+            qs = ModelClass.objects.filter(pk__gt=start_pk).order_by('pk')[:batch_size]
+
+
+    def lookup_enterprise_customer(enrollment_id):
+        EnterpriseCourseEnrollment = enterprise_course_enrollment_model()
+        EnterpriseCustomerUser = enterprise_customer_user_model()
+        EnterpriseCustomer = enterprise_customer_model()
+        try:
+            ece = EnterpriseCourseEnrollment.objects.get(pk=enrollment_id)
+            ecu = EnterpriseCustomerUser.objects.get(pk=ece.enterprise_customer_user_id)
+            ec = EnterpriseCustomer.objects.get(pk=ecu.enterprise_customer_id)
+            return ec
+        except ObjectDoesNotExist:
+            return None
+
+    def backfill_join_keys():
+        try:
+            for models_pair in MODELS.values():
+                ConfigModel, LearnerAuditModel = models_pair
+                LOGGER.info(f'{LearnerAuditModel.__name__}')
+                for audit_record_batch in batch_by_pk(LearnerAuditModel):
+                    for audit_record in audit_record_batch:
+                        LOGGER.info(f'{LearnerAuditModel.__name__} <{audit_record.pk}>')
+                        enterprise_customer = lookup_enterprise_customer(audit_record.enterprise_course_enrollment_id)
+                        if enterprise_customer is None:
+                            continue
+                        config = ConfigModel.objects.filter(enterprise_customer=enterprise_customer).first()
+                        if config is None:
+                            continue
+                        LOGGER.info(f'{LearnerAuditModel.__name__} <{audit_record.pk}> '
+                                    f'enterprise_customer_uuid={enterprise_customer.uuid}, '
+                                    f'plugin_configuration_id={config.id}')
+            for audit_record_batch in batch_by_pk(ContentMetadataItemTransmission):
+                for audit_record in audit_record_batch:
+                    LOGGER.info(f'ContentMetadataItemTransmission <{audit_record.pk}>')
+                    # if we cant lookup by code, skip
+                    channel_models = MODELS[audit_record.integrated_channel_code]
+                    if channel_models is None:
+                        continue
+                    ConfigModel = channel_models[0]
+                    if audit_record.enterprise_customer is None:
+                        continue
+                    config = ConfigModel.objects.filter(enterprise_customer=audit_record.enterprise_customer).first()
+                    LOGGER.info(f'ContentMetadataItemTransmission <{audit_record.pk}> '
+                                f'plugin_configuration_id={config.id}')            
+        except:
+            LOGGER.exception()
+
+    def handle(self, *args, **options):
+        """
+        Backfill missing audit record foreign keys.
+        """
+        # Ensure that we were given an api_user name, and that User exists.
+        api_username = options['api_user']
+        try:
+            User.objects.get(username=api_username)
+        except User.DoesNotExist as no_user_error:
+            raise CommandError(
+                _('A user with the username {username} was not found.').format(username=api_username)
+            ) from no_user_error
+
+        backfill_join_keys()
