68ad85078a9ab6ec43a1453af69b0492b8f0e1a8
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-03-30 18:59:15 +0500

    build: Running migrations on mysql8. (#1514)
    Co-authored-by: Muhammad Soban Javed <58461728+iamsobanjaved@users.noreply.github.com>

--- enterprise/settings/test.py
+++ enterprise/settings/test.py
@@ -5,6 +5,7 @@ In a real-world use case, apps in this project are installed into other
 Django applications, so these settings will not be used.
 """
 
+import os
 from os.path import abspath, dirname, join
 
 from celery import Celery
@@ -33,14 +34,14 @@ def root(*args):
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "default.db",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
-    }
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', 'default.db'),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
+    },
 }
 
 
