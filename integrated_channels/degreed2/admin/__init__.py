9e97f566a026092c41a622ccfbe9c86c8b30f986
John Nagro <jnagro@edx.org>
2022-04-18 14:42:19 -0400

    fix: configure django admin for degreed2 audit records
    - Degreed2EnterpriseCustomerConfiguration admin registartion / config
    
    ENT-5598

--- integrated_channels/degreed2/admin/__init__.py
+++ integrated_channels/degreed2/admin/__init__.py
@@ -3,9 +3,35 @@
 Django admin integration for configuring degreed app to communicate with Degreed systems.
 """
 
+from django.apps import apps
 from django.contrib import admin
+from django.core.exceptions import ObjectDoesNotExist
 
-from integrated_channels.degreed2.models import Degreed2EnterpriseCustomerConfiguration
+from integrated_channels.degreed2.models import (
+    Degreed2EnterpriseCustomerConfiguration,
+    Degreed2LearnerDataTransmissionAudit,
+)
+
+
+def enterprise_course_enrollment_model():
+    """
+    Returns the ``EnterpriseCourseEnrollment`` class.
+    """
+    return apps.get_model('enterprise', 'EnterpriseCourseEnrollment')
+
+
+def enterprise_customer_user_model():
+    """
+    Returns the ``EnterpriseCustomerUser`` class.
+    """
+    return apps.get_model('enterprise', 'EnterpriseCustomerUser')
+
+
+def enterprise_customer_model():
+    """
+    Returns the ``EnterpriseCustomer`` class.
+    """
+    return apps.get_model('enterprise', 'EnterpriseCustomer')
 
 
 @admin.register(Degreed2EnterpriseCustomerConfiguration)
@@ -47,3 +73,59 @@ class Degreed2EnterpriseCustomerConfigurationAdmin(admin.ModelAdmin):
                 being rendered with this admin form.
         """
         return obj.enterprise_customer.name
+
+
+@admin.register(Degreed2LearnerDataTransmissionAudit)
+class Degreed2LearnerDataTransmissionAuditAdmin(admin.ModelAdmin):
+    """
+    Django admin model for Degreed2LearnerDataTransmissionAudit.
+    """
+    list_display = (
+        "user_email",
+        "user_id",
+        "enterprise_course_enrollment_id",
+        "course_id",
+        "status",
+    )
+
+    raw_id_fields = (
+        "user",
+    )
+
+    readonly_fields = (
+        "enterprise_customer_name",
+    )
+
+    class Meta:
+        model = Degreed2LearnerDataTransmissionAudit
+
+    def user_email(self, obj):
+        """
+        Returns: the name for the attached EnterpriseCustomer.
+
+        Args:
+            obj: The instance of Degreed2LearnerDataTransmissionAudit
+                being rendered with this admin form.
+        """
+        return obj.user.email
+
+    def enterprise_customer_name(self, obj):
+        """
+        Returns: the name for the attached EnterpriseCustomer.
+        Args:
+            obj: The instance of Degreed2LearnerDataTransmissionAudit
+                being rendered with this admin form.
+        """
+
+        # a direct foreign key relationship is missing
+        # multiple queries here so, avoid adding it to list_display fields
+        EnterpriseCourseEnrollment = enterprise_course_enrollment_model()
+        EnterpriseCustomerUser = enterprise_customer_user_model()
+        EnterpriseCustomer = enterprise_customer_model()
+        try:
+            ece = EnterpriseCourseEnrollment.objects.get(pk=obj.enterprise_course_enrollment_id)
+            ecu = EnterpriseCustomerUser.objects.get(pk=ece.enterprise_customer_user_id)
+            ec = EnterpriseCustomer.objects.get(pk=ecu.enterprise_customer_id)
+            return ec.name
+        except ObjectDoesNotExist:
+            return None
