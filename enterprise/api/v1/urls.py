e82a7818d335f5b6058e84e1f92860fb669ca332
ruzniaiedm <ruzniaievdm@gmail.com>
2022-04-20 16:45:23 +0300

    refactor: Remove enterprise catalog V1 endpoints

--- enterprise/api/v1/urls.py
+++ enterprise/api/v1/urls.py
@@ -4,19 +4,32 @@ URL definitions for enterprise api version 1 endpoint.
 
 from rest_framework.routers import DefaultRouter
 
+from django.conf import settings
 from django.urls import re_path
 
 from enterprise.api.v1 import views
 
 router = DefaultRouter()
-router.register("enterprise_catalogs", views.EnterpriseCustomerCatalogViewSet, 'enterprise-catalogs')
+
+# Endpoints are deprecated. See https://github.com/openedx/public-engineering/issues/61
+# .. toggle_name: ENABLE_DEPRECATED_API_ENDPOINTS
+# .. toggle_implementation: DjangoSetting
+# .. toggle_default: True
+# .. toggle_description: If True, it adds an option to show/hide the deprecation api endpoints.
+# .. toggle_use_cases: open_edx
+# .. toggle_creation_date: 2022-04-26
+# .. toggle_target_removal_date: 2022-05-26
+# .. toggle_tickets: https://github.com/openedx/edx-enterprise/pull/1540
+if getattr(settings, 'ENABLE_DEPRECATED_API_ENDPOINTS', True):
+    router.register("enterprise_catalogs", views.EnterpriseCustomerCatalogViewSet, 'enterprise-catalogs')
+    router.register("enterprise-customer", views.EnterpriseCustomerViewSet, 'enterprise-customer')
+
 router.register("enterprise-course-enrollment", views.EnterpriseCourseEnrollmentViewSet, 'enterprise-course-enrollment')
 router.register(
     "licensed-enterprise-course-enrollment",
     views.LicensedEnterpriseCourseEnrollmentViewSet,
     'licensed-enterprise-course-enrollment'
 )
-router.register("enterprise-customer", views.EnterpriseCustomerViewSet, 'enterprise-customer')
 router.register("enterprise-learner", views.EnterpriseCustomerUserViewSet, 'enterprise-learner')
 router.register("pending-enterprise-learner", views.PendingEnterpriseCustomerUserViewSet, 'pending-enterprise-learner')
 router.register(
