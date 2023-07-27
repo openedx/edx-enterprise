381221797be69387b1f5c5832fbc9b3277f156e6
Alexander J Sheehan <asheehan@2u.com>
2023-07-27 16:17:07 +0000

    chore: updating requirements

--- enterprise/management/commands/manufacture_factory_data.py
+++ enterprise/management/commands/manufacture_factory_data.py
@@ -10,16 +10,17 @@ import factory
 from django.core.exceptions import FieldDoesNotExist, ImproperlyConfigured
 from django.core.management.base import BaseCommand, CommandError, SystemCheckError, handle_default_options
 from django.db import connections
+from django.db.models.fields.related import ForeignKey
 
 # We have to import the enterprise test factories to ensure it's loaded and found by __subclasses__
-from test_utils import factories as _
+from test_utils import factories  # pylint: disable=unused-import
 
 # To ensure factories outside of the enterprise package are loaded and found by the script,
 # add any additionally desired factories as an import to this file. Make sure to catch the ImportError
 # incase other consumers of the command do not have the same factories installed.
 # For example:
 try:
-    import common.djangoapps.student.test.factories as _
+    import common.djangoapps.student.test.factories  # pylint: disable=unused-import
 except ImportError:
     pass
 
@@ -70,7 +71,8 @@ class Command(BaseCommand):
     def run_from_argv(self, argv):
         """
         Re-implemented from https://github.com/django/django/blob/main/django/core/management/base.py#L395 in order to
-        support individual field customization.
+        support individual field customization. We will need to keep this method up to date with our current version of
+        Django BaseCommand.
 
         Uses ``parse_known_args`` instead of ``parse_args`` to not throw an error when encountering unknown arguments
 
@@ -79,8 +81,14 @@ class Command(BaseCommand):
         self._called_from_command_line = True
         parser = self.create_parser(argv[0], argv[1])
         options, unknown = parser.parse_known_args(argv[2:])
-        # pylint: disable=attribute-defined-outside-init
-        self.custom_attributes = unknown
+
+        # Add the unknowns into the options for use of the handle method
+        paired_unknowns = pairwise(unknown)
+        field_customizations = {}
+        for field, value in paired_unknowns:
+            field_customizations[field.strip("--")] = value
+        options.field_customizations = field_customizations
+
         cmd_options = vars(options)
         # Move positional args out of options to mimic legacy optparse
         args = cmd_options.pop("args", ())
@@ -113,9 +121,14 @@ class Command(BaseCommand):
             log.error("Did not receive a model")
             raise CommandError("Did not receive a model")
 
+        # Convert to Pascal case if the provided name is snake case/is all lowercase
         path_of_model = options.get('model').split(".")
-        provided_model = '.'.join(path_of_model[:-1]) + '.' + convert_to_pascal(path_of_model[-1])
+        if '_' in path_of_model[-1] or path_of_model[-1].islower():
+            last_path = convert_to_pascal(path_of_model[-1])
+        else:
+            last_path = path_of_model[-1]
 
+        provided_model = '.'.join(path_of_model[:-1]) + '.' + last_path
         # Get all installed/imported factories
         factories_list = all_subclasses(factory.django.DjangoModelFactory)
         # Find the factory that matches the provided model
@@ -123,18 +136,29 @@ class Command(BaseCommand):
             factory_model = potential_factory._meta.model
             if f"{factory_model.__module__}.{factory_model.__name__}" == provided_model:
                 packed_factory_kwargs = {}
-                paired_attributes = pairwise(self.custom_attributes)
+                field_customizations = options.get('field_customizations')
                 # Confirm that the provided custom attributes are valid fields on the provided model
-                for field, value in paired_attributes:
-                    striped_field = field.strip("--")
+                for field, value in field_customizations.items():
+                    stripped_field = field.strip("--")
                     try:
-                        factory_model._meta.get_field(striped_field)
+                        f = factory_model._meta.get_field(stripped_field)
+                        # Test factories won't accept a FK PK value, we have to supply the object itself.
+                        # This also gives us an opportunity to validate that the provided value is a valid.
+                        if isinstance(f, ForeignKey):
+                            try:
+                                related_object = f.related_model.objects.get(pk=value)
+                                value = related_object
+                            except f.related_model.DoesNotExist:
+                                raise CommandError(
+                                    f'Could not find record: {value} for model: {stripped_field}'
+                                )
+
                     except FieldDoesNotExist as exc:
                         log.error(f"Provided field: {field} does not exist on {factory_model}")
                         raise CommandError(f'Provided field: {field} does not exist on {factory_model}') from exc
                     # Now that we know the custom attribute exists as a field on the provided model,
                     # add it to the kwargs dictionary for the record generation
-                    packed_factory_kwargs[striped_field] = value
+                    packed_factory_kwargs[stripped_field] = value
 
                 # Unpack the custom attributes into the kwargs of the factory, generate a record and return the pk
                 return str(potential_factory(**packed_factory_kwargs).pk)
