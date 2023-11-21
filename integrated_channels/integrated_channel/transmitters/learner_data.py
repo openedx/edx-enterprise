d6554eb1334163e95b4f6df6719043a89d8232db
mueez.khan <mueez.khan@arbisoft.com>
2023-11-22 01:44:55 +0500

    refactor: made the comment explaining check, more clear

--- integrated_channels/integrated_channel/transmitters/learner_data.py
+++ integrated_channels/integrated_channel/transmitters/learner_data.py
@@ -313,7 +313,8 @@ class LearnerTransmitter(Transmitter, ChannelSettingsMixin):
                 not hasattr(self.enterprise_configuration, 'enable_incomplete_progress_transmission')
                 or not self.enterprise_configuration.enable_incomplete_progress_transmission
             ):
-                # The user has not completed the course, so we shouldn't send a completion status call
+                # The user has not completed the course and enable_incomplete_progress_transmission is not set,
+                # so we shouldn't send a completion status call
                 remote_id = getattr(learner_data, kwargs.get('remote_user_id'))
                 encoded_serialized_payload = encode_data_for_logging(serialized_payload)
                 LOGGER.info(generate_formatted_log(
