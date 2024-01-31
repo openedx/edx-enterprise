56b8080d6b9061b6d78b96b9d3461bc7922b39a6
zamanafzal <zamanafzal@gmail.com>
2024-01-31 18:15:32 +0500

    feat: Handle management command to fix `LearnerDataTransmissionAudit` table records for cornersotne.

--- integrated_channels/integrated_channel/management/commands/remove_duplicate_learner_transmission_audit_records.py
+++ integrated_channels/integrated_channel/management/commands/remove_duplicate_learner_transmission_audit_records.py
@@ -33,35 +33,73 @@ class Command(BaseCommand):
             ('moodle', 'MoodleLearnerDataTransmissionAudit'),
             ('blackboard', 'BlackboardLearnerDataTransmissionAudit'),
             ('cornerstone', 'CornerstoneLearnerDataTransmissionAudit'),
-            ('canvas', 'CanvasLearnerAssessmentDataTransmissionAudit'),
+            ('canvas', 'CanvasLearnerDataTransmissionAudit'),
             ('degreed2', 'Degreed2LearnerDataTransmissionAudit'),
             ('sap_success_factors', 'SapSuccessFactorsLearnerDataTransmissionAudit'),
         ]
         for app_label, model_name in channel_learner_audit_models:
             model_class = apps.get_model(app_label=app_label, model_name=model_name)
 
-            latest_records_without_prefix = (
-                model_class.objects.exclude(course_id__startswith='course-v1:')
-                .values('enterprise_course_enrollment_id').annotate(most_recent_transmission_id=Max('id'))
-            )
+            if model_name == 'CornerstoneLearnerDataTransmissionAudit':
+                # Special handling for CornerstoneLearnerDataTransmissionAudit
+                duplicate_records_to_delete = (
+                    model_class.objects
+                    .filter(user_id__isnull=False, course_id__isnull=False)
+                    .exclude(course_id__startswith='course-v1:')
+                    .values('user_id', 'course_id')
+                    .annotate(most_recent_transmission_id=Max('id'))
+                )
 
-            LOGGER.info(
-                f'{app_label} channel has {latest_records_without_prefix.count()} records without prefix'
-            )
+                LOGGER.info(
+                    f'{app_label} channel has {duplicate_records_to_delete.count()} duplicate records to delete'
+                )
 
-            # Delete all duplicate records for each enterprise_course_enrollment_id
-            with transaction.atomic():
-                for entry in latest_records_without_prefix:
-                    enterprise_course_enrollment_id = entry['enterprise_course_enrollment_id']
-                    most_recent_transmission_id = entry['most_recent_transmission_id']
+                # Delete all duplicate records based on the latest transmission id
+                with transaction.atomic():
+                    for entry in duplicate_records_to_delete:
+                        user_id = entry['user_id']
+                        course_id = entry['course_id']
+                        most_recent_transmission_id = entry['most_recent_transmission_id']
 
-                    # Delete all records except the latest one without "course-v1:"
-                    duplicate_records_to_delete = (
-                        model_class.objects
-                        .filter(enterprise_course_enrollment_id=enterprise_course_enrollment_id)
-                        .exclude(id=most_recent_transmission_id)
-                    )
-                    LOGGER.info(
-                        f'{app_label} channel - {duplicate_records_to_delete.count()} duplicate records are deleted'
-                    )
-                    duplicate_records_to_delete.delete()
+                        # Delete all records except the latest one without "course-v1:"
+                        duplicate_records = (
+                            model_class.objects
+                            .filter(user_id=user_id, course_id=course_id)
+                            .exclude(id=most_recent_transmission_id)
+                        )
+                        LOGGER.info(
+                            f'{app_label} channel - {duplicate_records.count()} duplicate records for user id {user_id}'
+                            f' and course id {course_id} are deleted'
+                        )
+                        duplicate_records.delete()
+
+            else:
+                # Default handling for other channels
+                duplicate_records_to_delete = (
+                    model_class.objects
+                    .exclude(course_id__startswith='course-v1:')
+                    .values('enterprise_course_enrollment_id')
+                    .annotate(most_recent_transmission_id=Max('id'))
+                )
+
+                LOGGER.info(
+                    f'{app_label} channel has {duplicate_records_to_delete.count()} duplicate records to delete'
+                )
+
+                # Delete all duplicate records based on the latest transmission id
+                with transaction.atomic():
+                    for entry in duplicate_records_to_delete:
+                        enterprise_course_enrollment_id = entry['enterprise_course_enrollment_id']
+                        most_recent_transmission_id = entry['most_recent_transmission_id']
+
+                        # Delete all records except the latest one without "course-v1:"
+                        duplicate_records = (
+                            model_class.objects
+                            .filter(enterprise_course_enrollment_id=enterprise_course_enrollment_id)
+                            .exclude(id=most_recent_transmission_id)
+                        )
+                        LOGGER.info(
+                            f'{app_label} channel - {duplicate_records.count()} duplicate records for enrollment id'
+                            f' {enterprise_course_enrollment_id} are deleted.'
+                        )
+                        duplicate_records.delete()
