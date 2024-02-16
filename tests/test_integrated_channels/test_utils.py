ba92fcd0232959d1df1cb9815b24589b1855038b
MueezKhan246 <mueez.khan@arbisoft.com>
2024-02-16 13:30:38 +0500

    test: fixing test case after addition of channel_name

--- tests/test_integrated_channels/test_utils.py
+++ tests/test_integrated_channels/test_utils.py
@@ -359,26 +359,26 @@ class TestIntegratedChannelsUtils(unittest.TestCase):
 
         # Test with dict input
         stringified_data = utils.stringify_and_store_api_record(
-            "Customer", 123, "/endpoint", data, 1.23, 200, "response"
+            "Customer", 123, "/endpoint", data, 1.23, 200, "response", 'integrated_channel_name'
         )
         assert stringified_data == json.dumps(data)
 
         # Test with int input
         stringified_int = utils.stringify_and_store_api_record(
-            "Customer", 123, "/endpoint", 123, 1.23, 200, "response"
+            "Customer", 123, "/endpoint", 123, 1.23, 200, "response", 'integrated_channel_name'
         )
         assert stringified_int == "123"
 
         # Test with tuple input
         data_tuple = (1, 2, "hello")
         stringified_tuple = utils.stringify_and_store_api_record(
-            "Customer", 123, "/endpoint", data_tuple, 1.23, 200, "response"
+            "Customer", 123, "/endpoint", data_tuple, 1.23, 200, "response", 'integrated_channel_name'
         )
         assert stringified_tuple == json.dumps(data_tuple)
 
         # Test with list input
         data_list = [1, 2, "world"]
         stringified_list = utils.stringify_and_store_api_record(
-            "Customer", 123, "/endpoint", data_list, 1.23, 200, "response"
+            "Customer", 123, "/endpoint", data_list, 1.23, 200, "response", 'integrated_channel_name'
         )
         assert stringified_list == json.dumps(data_list)
