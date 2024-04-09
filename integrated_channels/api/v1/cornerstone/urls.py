fb475c18e58889ef57318545819d29f0f074a284
Sameen Fatima <sameen.fatima@A006-01036.local>
2024-04-09 13:52:05 +0500

    feat: save cornerstone learner information
    fixed and added tests
    
    fixed quality failures

--- integrated_channels/api/v1/cornerstone/urls.py
+++ integrated_channels/api/v1/cornerstone/urls.py
@@ -4,9 +4,17 @@
 
 from rest_framework import routers
 
-from .views import CornerstoneConfigurationViewSet
+from django.urls import path
+
+from .views import CornerstoneConfigurationViewSet, CornerstoneLearnerInformationView
 
 app_name = 'cornerstone'
 router = routers.DefaultRouter()
 router.register(r'configuration', CornerstoneConfigurationViewSet, basename="configuration")
-urlpatterns = router.urls
+urlpatterns = [
+    path('save-learner-information', CornerstoneLearnerInformationView.as_view(),
+         name='save-learner-information'
+         ),
+]
+
+urlpatterns += router.urls
