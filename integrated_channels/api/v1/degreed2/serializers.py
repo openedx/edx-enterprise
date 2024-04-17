43ac63e977b732f1ee03d7f8ae283336d69f6b30
MueezKhan246 <93375917+MueezKhan246@users.noreply.github.com>
2024-04-17 06:56:57 +0500

    Degreed replacing encrypted fields with non encrypted (#2063)
    * feat: replacing non encrypted fields of degreed config model with encrypted ones

--- integrated_channels/api/v1/degreed2/serializers.py
+++ integrated_channels/api/v1/degreed2/serializers.py
@@ -1,6 +1,8 @@
 """
 Serializer for Degreed2 configuration.
 """
+from rest_framework import serializers
+
 from integrated_channels.api.serializers import EnterpriseCustomerPluginConfigSerializer
 from integrated_channels.degreed2.models import Degreed2EnterpriseCustomerConfiguration
 
@@ -11,7 +13,11 @@ class Degreed2ConfigSerializer(EnterpriseCustomerPluginConfigSerializer):
         extra_fields = (
             'client_id',
             'client_secret',
+            'encrypted_client_id',
+            'encrypted_client_secret',
             'degreed_base_url',
             'degreed_token_fetch_base_url',
         )
         fields = EnterpriseCustomerPluginConfigSerializer.Meta.fields + extra_fields
+        encrypted_client_id = serializers.CharField(required=False, allow_blank=False, read_only=False)
+        encrypted_client_secret = serializers.CharField(required=False, allow_blank=False, read_only=False)
