8f1d7e18af169e2acd2b625427b2bd74aa42256a
MueezKhan246 <93375917+MueezKhan246@users.noreply.github.com>
2024-04-25 17:00:40 +0500

    Remove degreed unencrypted columns Ent8009 (#2069)
    * feat: removed unencrypted columns of user data credentials in degreed config ENT 8009

--- tests/test_integrated_channels/test_api/test_degreed2/test_views.py
+++ tests/test_integrated_channels/test_api/test_degreed2/test_views.py
@@ -64,9 +64,8 @@ class Degreed2ConfigurationViewSetTests(APITest):
         url = reverse('api:v1:degreed2:configuration-detail', args=[self.degreed2_config.id])
         response = self.client.get(url)
         data = json.loads(response.content.decode('utf-8'))
+
         self.assertEqual(data.get('degreed_base_url'), self.degreed2_config.degreed_base_url)
-        self.assertEqual(data.get('client_id'), self.degreed2_config.client_id)
-        self.assertEqual(data.get('client_secret'), self.degreed2_config.client_secret)
         self.assertEqual(data.get('degreed_token_fetch_base_url'), self.degreed2_config.degreed_token_fetch_base_url)
         self.assertEqual(data.get('enterprise_customer'),
                          str(self.degreed2_config.enterprise_customer.uuid))
@@ -82,14 +81,14 @@ class Degreed2ConfigurationViewSetTests(APITest):
             'degreed_base_url': 'http://testing2',
             'degreed_token_fetch_base_url': 'foobar',
             'enterprise_customer': ENTERPRISE_ID,
-            'client_id': 'testing',
-            'client_secret': 'secret',
+            'encrypted_client_id': 'testing',
+            'encrypted_client_secret': 'secret',
         }
         response = self.client.put(url, payload)
         self.degreed2_config.refresh_from_db()
         self.assertEqual(self.degreed2_config.degreed_base_url, 'http://testing2')
-        self.assertEqual(self.degreed2_config.client_id, 'testing')
-        self.assertEqual(self.degreed2_config.client_secret, 'secret')
+        self.assertEqual(self.degreed2_config.decrypted_client_id, 'testing')
+        self.assertEqual(self.degreed2_config.decrypted_client_secret, 'secret')
         self.assertEqual(self.degreed2_config.degreed_token_fetch_base_url, 'foobar')
         self.assertEqual(str(self.degreed2_config.enterprise_customer.uuid), ENTERPRISE_ID)
         self.assertEqual(response.status_code, 200)
@@ -134,8 +133,8 @@ class Degreed2ConfigurationViewSetTests(APITest):
         )
         url = reverse('api:v1:degreed2:configuration-list')
 
-        self.degreed2_config.client_id = ''
-        self.degreed2_config.client_secret = ''
+        self.degreed2_config.decrypted_client_id = ''
+        self.degreed2_config.decrypted_client_secret = ''
         self.degreed2_config.degreed_base_url = ''
         self.degreed2_config.degreed_token_fetch_base_url = ''
         self.degreed2_config.save()
@@ -143,7 +142,7 @@ class Degreed2ConfigurationViewSetTests(APITest):
         response = self.client.get(url)
         data = json.loads(response.content.decode('utf-8')).get('results')
         missing, _ = data[0].get('is_valid')
-        assert missing.get('missing') == ['client_id', 'client_secret', 'degreed_base_url']
+        assert missing.get('missing') == ['decrypted_client_id', 'decrypted_client_secret', 'degreed_base_url']
 
         self.degreed2_config.degreed_base_url = 'eww'
         self.degreed2_config.display_name = 'thisisagrosslongdisplayname'
@@ -155,8 +154,8 @@ class Degreed2ConfigurationViewSetTests(APITest):
         assert incorrect.get('incorrect') == ['degreed_base_url', 'display_name']
 
         # Add a client id and proper url and assert that is_valid now passes
-        self.degreed2_config.client_id = 'ayylmao'
-        self.degreed2_config.client_secret = 'whatsup'
+        self.degreed2_config.decrypted_client_id = 'ayylmao'
+        self.degreed2_config.decrypted_client_secret = 'whatsup'
         self.degreed2_config.degreed_base_url = 'http://nice.com'
         self.degreed2_config.display_name = 'lovely'
         self.degreed2_config.degreed_token_fetch_base_url = 'http://hey.com'
