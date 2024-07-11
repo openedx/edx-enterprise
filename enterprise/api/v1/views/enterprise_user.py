c915dbe41e68ccd7051f67ec77ca81b73b5536c8
Prashant Makwana <pmakwana@2u.com>
2024-07-11 20:26:19 +0000

    feat: updating enterprise-user endpoint

--- enterprise/api/v1/views/enterprise_user.py
+++ enterprise/api/v1/views/enterprise_user.py
@@ -18,7 +18,7 @@ class EnterpriseUserViewSet(EnterpriseReadWriteModelViewSet):
     """
     API views for the ``enterprise-user`` API endpoint.
     """
-    queryset = models.PendingEnterpriseCustomerAdminUser.objects.all()
+    queryset = models.EnterpriseCustomer.active_customers.all()
     filter_backends = (filters.OrderingFilter, DjangoFilterBackend,)
     serializer_class = serializers.EnterpriseUserSerializer
     permission_classes = (permissions.IsAuthenticated,)
