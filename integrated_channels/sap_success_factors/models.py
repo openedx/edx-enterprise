3bb3b5dc6fc813f68f83fa2cc1dc4869431c6f62
MueezKhan246 <mueez.khan@arbisoft.com>
2024-09-04 19:36:27 +0500

    feat: added encrypted client secret for SAP config

--- integrated_channels/sap_success_factors/models.py
+++ integrated_channels/sap_success_factors/models.py
@@ -122,6 +122,18 @@ class SAPSuccessFactorsEnterpriseCustomerConfiguration(EnterpriseCustomerPluginC
         verbose_name="Client Secret",
         help_text=_("OAuth client secret.")
     )
+    
+    decrypted_secret = EncryptedCharField(
+        max_length=255,
+        blank=True,
+        default='',
+        verbose_name="Encrypted Client Secret",
+        help_text=_(
+            "The encrypted OAuth client secret."
+            " It will be encrypted when stored in the database."
+        ),
+        null=True
+    )
 
     user_type = models.CharField(
         max_length=20,
