6b494c6ccdf3982d03bad3ded9ed668f506094c5
Jesper Hodge <jhodge@outlook.de>
2025-02-27 18:36:08 -0500

    fix: add user relation correctly

--- enterprise/models.py
+++ enterprise/models.py
@@ -1152,7 +1152,7 @@ class EnterpriseCustomerUser(TimeStampedModel):
                 enterprise_customer=self.enterprise_customer,
             )
 
-        self.user_fk = self.user_id
+        self.user_fk = self.user
 
         return super().save(*args, **kwargs)
 
