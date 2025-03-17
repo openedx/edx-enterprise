ce174c9f45535d1d5538869c062fa014215967d5
Alexander Dusenbery <adusenbery@2u.com>
2025-03-17 10:39:54 -0400

    fix: defensively import edx-braze-client.
    ENT-10147 | defensively import edx-braze-client so that
    tests in the edx-platform CI context won't raise ``ImportErrors``.

--- enterprise/api_client/braze.py
+++ enterprise/api_client/braze.py
@@ -3,36 +3,41 @@ API clients to communicate with Braze services.
 """
 import logging
 
-from braze.client import BrazeClient
-
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
+
+try:
+    from braze.client import BrazeClient
+except ImportError:
+    logger.warning('Cannot import BrazeClient, calls to Braze will not work')
+    BrazeClient = None
 
 ENTERPRISE_BRAZE_ALIAS_LABEL = 'Enterprise'  # Do Not change this, this is consistent with other uses across edX repos.
 # https://www.braze.com/docs/api/endpoints/user_data/post_user_identify/
 MAX_NUM_IDENTIFY_USERS_ALIASES = 50
 
 
-class BrazeAPIClient(BrazeClient):
+class BrazeAPIClient(BrazeClient or object):
     """
     API client for calls to Braze.
     """
     def __init__(self):
-        braze_api_key = getattr(settings, 'ENTERPRISE_BRAZE_API_KEY', None)
-        braze_api_url = getattr(settings, 'EDX_BRAZE_API_SERVER', None)
-        required_settings = ['ENTERPRISE_BRAZE_API_KEY', 'EDX_BRAZE_API_SERVER']
-        for setting in required_settings:
-            if not getattr(settings, setting, None):
-                msg = f'Missing {setting} in settings required for Braze API Client.'
-                logger.error(msg)
-                raise ValueError(msg)
+        if BrazeClient:
+            braze_api_key = getattr(settings, 'ENTERPRISE_BRAZE_API_KEY', None)
+            braze_api_url = getattr(settings, 'EDX_BRAZE_API_SERVER', None)
+            required_settings = ['ENTERPRISE_BRAZE_API_KEY', 'EDX_BRAZE_API_SERVER']
+            for setting in required_settings:
+                if not getattr(settings, setting, None):
+                    msg = f'Missing {setting} in settings required for Braze API Client.'
+                    logger.error(msg)
+                    raise ValueError(msg)
 
-        super().__init__(
-            api_key=braze_api_key,
-            api_url=braze_api_url,
-            app_id='',
-        )
+            super().__init__(
+                api_key=braze_api_key,
+                api_url=braze_api_url,
+                app_id='',
+            )
 
     def generate_mailto_link(self, emails):
         """
