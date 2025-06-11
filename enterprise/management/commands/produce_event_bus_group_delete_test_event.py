910c1acd3d6ef84209f8f681e16ec680e6fd9e77
Jesper Hodge <jhodge@outlook.de>
2025-06-11 13:21:49 -0400

    chore: add debugging command

---
+++ enterprise/management/commands/produce_event_bus_group_delete_test_event.py
@@ -0,0 +1,44 @@
+"""
+Produce a single event for enterprise-specific testing or health checks.
+
+Implements required ``APP.management.commands.*.Command`` structure.
+"""
+import logging
+import uuid
+
+import attr
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from edx_event_bus_kafka.internal.producer import create_producer
+from openedx_events.data import EventsMetadata
+from openedx_events.tooling import OpenEdxPublicSignal
+from enterprise.event_bus import send_enterprise_group_deleted_event
+
+logger = logging.getLogger(__name__)
+
+
+# First define the topic that our consumer will subscribe to.
+ENTERPRISE_CORE_TOPIC = getattr(settings, 'EVENT_BUS_ENTERPRISE_CORE_TOPIC', 'enterprise-core')
+
+class Command(BaseCommand):
+    """
+    Management command to produce a test event to the event bus.
+    """
+    help = """
+    Produce a single ping event to the configured test topic.
+
+    example:
+        ./manage.py produce_enterprise_ping_event
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--topic', nargs=1, required=False,
+            help="Optional topic to produce to (without environment prefix)",
+        )
+
+    def handle(self, *args, **options):
+        try:
+            send_enterprise_group_deleted_event()
+        except Exception:  # pylint: disable=broad-exception-caught
+            logger.exception("Error producing Kafka event")
