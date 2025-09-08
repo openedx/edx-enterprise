89e034cab73989e2afabe84f0b023027a1aa328b
zamanafzal <zamanafzal@gmail.com>
2025-09-08 19:42:29 +0500

    fix: Moodle configuration updates were not being saved correctly

--- integrated_channels/api/v1/moodle/serializers.py
+++ integrated_channels/api/v1/moodle/serializers.py
@@ -8,18 +8,53 @@ from integrated_channels.moodle.models import MoodleEnterpriseCustomerConfigurat
 
 
 class MoodleConfigSerializer(EnterpriseCustomerPluginConfigSerializer):
+    """
+    Serializer for Moodle Enterprise Customer Configuration.
+    Handles token or username/password authentication methods.
+    """
     class Meta:
         model = MoodleEnterpriseCustomerConfiguration
         extra_fields = (
             'moodle_base_url',
             'service_short_name',
             'category_id',
-            'encrypted_username',
-            'encrypted_password',
-            'encrypted_token',
+            'username',
+            'password',
+            'token',
         )
         fields = EnterpriseCustomerPluginConfigSerializer.Meta.fields + extra_fields
 
-    encrypted_password = serializers.CharField(required=False, allow_blank=False, read_only=False)
-    encrypted_username = serializers.CharField(required=False, allow_blank=False, read_only=False)
-    encrypted_token = serializers.CharField(required=False, allow_blank=False, read_only=False)
+    username = serializers.CharField(required=False, allow_blank=True, write_only=True)
+    password = serializers.CharField(required=False, allow_blank=True, write_only=True)
+    token = serializers.CharField(required=False, allow_blank=True, write_only=True)
+
+    def _handle_credentials(self, instance, username=None, password=None, token=None):
+        """Helper method to handle credential updates."""
+        if token is not None:
+            instance.decrypted_token = token
+            instance.decrypted_username = None
+            instance.decrypted_password = None
+        elif username is not None and password is not None:
+            instance.decrypted_token = None
+            instance.decrypted_username = username
+            instance.decrypted_password = password
+
+    def create(self, validated_data):
+        username = validated_data.pop('username', None)
+        password = validated_data.pop('password', None)
+        token = validated_data.pop('token', None)
+
+        instance = super().create(validated_data)
+        self._handle_credentials(instance, username, password, token)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        username = validated_data.pop('username', None)
+        password = validated_data.pop('password', None)
+        token = validated_data.pop('token', None)
+
+        instance = super().update(instance, validated_data)
+        self._handle_credentials(instance, username, password, token)
+        instance.save()
+        return instance
