e0637d0e92a7316ea013fdc2ba41fc69978ee8e1
sjasti-sonata-svg <sjasti-sonata@2u.com>
2026-07-22 23:05:51 +0530

    fix: hard delete EnterpriseGroup records instead of soft deleting them (ENT-12073) (#2660)
    Soft-deleted groups remained in the database and collided with the
    unique_together constraint on (name, enterprise_customer), blocking
    customers from reusing a deleted group's name even though the group
    was not visible in the admin. Groups are now hard deleted, relying on
    existing django-simple-history records for the audit trail. Group
    creation also clears out any legacy soft-deleted row for the same
    name/customer so pre-existing zombie rows don't block re-creation.
    
    Co-authored-by: sjasti-sonata-svg <sjasti-sonata-svg@users.noreply.github.com>

--- tests/test_admin/test_admin.py
+++ tests/test_admin/test_admin.py
@@ -6,8 +6,8 @@ from django.db import connection
 from django.test import RequestFactory, TestCase
 from django.test.utils import CaptureQueriesContext
 
-from enterprise.admin import EnterpriseGroupMembershipAdmin
-from enterprise.models import EnterpriseGroupMembership
+from enterprise.admin import EnterpriseGroupAdmin, EnterpriseGroupMembershipAdmin
+from enterprise.models import EnterpriseGroup, EnterpriseGroupMembership
 from integrated_channels.integrated_channel.admin import IntegratedChannelAPIRequestLogAdmin
 from integrated_channels.integrated_channel.models import IntegratedChannelAPIRequestLogs
 from test_utils import factories
@@ -123,3 +123,59 @@ class EnterpriseGroupMembershipAdminTest(TestCase):
         self.assertEqual(len(results), 2)
         # is_removed value should differ between the two results
         self.assertNotEqual(results[0].is_removed, results[1].is_removed)
+
+
+class EnterpriseGroupAdminTest(TestCase):
+    """
+    Test the admin functionality for the EnterpriseGroup model.
+    """
+
+    def setUp(self):
+        """
+        Set up the test environment by creating a test admin instance and sample data.
+        """
+        self.site = AdminSite()
+        self.admin = EnterpriseGroupAdmin(EnterpriseGroup, self.site)
+
+    def test_delete_queryset_hard_deletes(self):
+        """
+        Test that the admin's bulk "Delete selected" action hard deletes groups,
+        instead of soft deleting them via the default bulk queryset delete.
+        """
+        group_1 = factories.EnterpriseGroupFactory()
+        group_2 = factories.EnterpriseGroupFactory()
+        queryset = EnterpriseGroup.objects.filter(uuid__in=[group_1.uuid, group_2.uuid])
+
+        self.admin.delete_queryset(RequestFactory().get("/"), queryset)
+
+        self.assertEqual(EnterpriseGroup.all_objects.filter(uuid__in=[group_1.uuid, group_2.uuid]).count(), 0)
+
+    def test_save_model_purges_legacy_soft_deleted_group_on_create(self):
+        """
+        Test that saving a new group via the admin purges any legacy soft-deleted
+        group with the same name/customer, so it doesn't hit a raw IntegrityError.
+        """
+        legacy_group = factories.EnterpriseGroupFactory(name='foobar')
+        legacy_group.is_removed = True
+        legacy_group.save()
+
+        new_group = EnterpriseGroup(name='foobar', enterprise_customer=legacy_group.enterprise_customer)
+        self.admin.save_model(RequestFactory().get("/"), new_group, form=None, change=False)
+
+        self.assertEqual(EnterpriseGroup.all_objects.filter(name='foobar', is_removed=True).count(), 0)
+        self.assertEqual(EnterpriseGroup.objects.filter(name='foobar', uuid=new_group.uuid).count(), 1)
+
+    def test_save_model_does_not_purge_on_change(self):
+        """
+        Test that editing an existing group via the admin does not run the legacy
+        soft-deleted group purge, since that's only relevant on creation.
+        """
+        legacy_group = factories.EnterpriseGroupFactory(name='foobar')
+        legacy_group.is_removed = True
+        legacy_group.save()
+
+        existing_group = factories.EnterpriseGroupFactory(name='existing-group')
+        existing_group.name = 'renamed-group'
+        self.admin.save_model(RequestFactory().get("/"), existing_group, form=None, change=True)
+
+        self.assertEqual(EnterpriseGroup.all_objects.filter(name='foobar', is_removed=True).count(), 1)
